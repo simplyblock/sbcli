c0e4b2b0d1e71224ef2c74bf41e9e4a1d927a28d
hamdy-simplyblock <hamdy@simplyblock.io>
2024-02-15 20:37:44 +0300

    Fix k8s spdk deploy to remove previous spdk deployment
    - Refactor caching node k8s code

--- simplyblock_web/blueprints/caching_node_ops_k8s.py
+++ simplyblock_web/blueprints/caching_node_ops_k8s.py
@@ -1,11 +1,9 @@
 #!/usr/bin/env python
 # encoding: utf-8
-import json
 import logging
 import math
 import os
 import time
-import subprocess
 
 import cpuinfo
 import yaml
@@ -13,10 +11,11 @@ from kubernetes import client, config
 from flask import Blueprint
 from flask import request
 from kubernetes.client import ApiException
+from jinja2 import Environment, FileSystemLoader
 
 from simplyblock_core import constants
 
-from simplyblock_web import utils
+from simplyblock_web import utils, node_utils
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
@@ -37,109 +36,23 @@ TOP_DIR = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
 spdk_deploy_yaml = os.path.join(TOP_DIR, 'static/deploy_spdk.yaml')
 
 
-def run_command(cmd):
-    process = subprocess.Popen(
-        cmd.split(), stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    stdout, stderr = process.communicate()
-    return stdout.strip().decode("utf-8"), stderr.strip(), process.returncode
-
-
-def _get_spdk_pcie_list():  # return: ['0000:00:1e.0', '0000:00:1f.0']
-    out, err, _ = run_command("ls /sys/bus/pci/drivers/uio_pci_generic")
-    spdk_pcie_list = [line for line in out.split() if line.startswith("0000")]
-    logger.debug(spdk_pcie_list)
-    return spdk_pcie_list
-
-
-def _get_nvme_pcie_list():  # return: ['0000:00:1e.0', '0000:00:1f.0']
-    out, err, _ = run_command("ls /sys/bus/pci/drivers/nvme")
-    spdk_pcie_list = [line for line in out.split() if line.startswith("0000")]
-    logger.debug(spdk_pcie_list)
-    return spdk_pcie_list
-
-
-def get_nvme_pcie():
-    # Returns a list of nvme pci address and vendor IDs,
-    # each list item is a tuple [("PCI_ADDRESS", "VENDOR_ID:DEVICE_ID")]
-    stream = os.popen("lspci -Dnn | grep -i nvme")
-    ret = stream.readlines()
-    devs = []
-    for line in ret:
-        line_array = line.split()
-        devs.append((line_array[0], line_array[-1][1:-1]))
-    return devs
-
-
-def _get_nvme_devices():
-    out, err, _ = run_command("nvme list -v -o json")
-    data = json.loads(out)
-    logger.debug("nvme list:")
-    logger.debug(data)
-
-    devices = []
-    if data and 'Devices' in data:
-        for host in data['Devices']:
-            for dev in host['Subsystems']:
-                if 'Controllers' in dev and dev['Controllers']:
-                    controller = dev['Controllers'][0]
-                    namespace = None
-                    if "Namespaces" in dev and dev['Namespaces']:
-                        namespace = dev['Namespaces'][0]
-                    elif controller and controller["Namespaces"]:
-                        namespace = controller['Namespaces'][0]
-                    if namespace:
-                        devices.append({
-                            'nqn': dev['SubsystemNQN'],
-                            'size': namespace['PhysicalSize'],
-                            'sector_size': namespace['SectorSize'],
-                            'device_name': namespace['NameSpace'],
-                            'device_path': "/dev/"+namespace['NameSpace'],
-                            'controller_name': controller['Controller'],
-                            'address': controller['Address'],
-                            'transport': controller['Transport'],
-                            'model_id': controller['ModelNumber'],
-                            'serial_number': controller['SerialNumber']})
-    return devices
-
-
-def get_nics_data():
-    out, _, _ = run_command("ip -j address show")
-    data = json.loads(out)
-    logger.debug("ifaces")
-    logger.debug(data)
-
-    def _get_ip4_address(list_of_addr):
-        if list_of_addr:
-            for data in list_of_addr:
-                if data['family'] == 'inet':
-                    return data['local']
-        return ""
-
-    devices = {i["ifname"]: i for i in data}
-    iface_list = {}
-    for nic in devices:
-        device = devices[nic]
-        iface = {
-            'name': device['ifname'],
-            'ip': _get_ip4_address(device['addr_info']),
-            'status': device['operstate'],
-            'net_type': device['link_type']}
-        iface_list[nic] = iface
-    return iface_list
-
-
-def _get_spdk_devices():
-    return []
+cpu_info = cpuinfo.get_cpu_info()
+hostname, _, _ = node_utils.run_command("hostname -s")
+system_id = ""
+try:
+    system_id, _, _ = node_utils.run_command("dmidecode -s system-uuid")
+except:
+    pass
 
 
 @bp.route('/scan_devices', methods=['GET'])
 def scan_devices():
     run_health_check = request.args.get('run_health_check', default=False, type=bool)
     out = {
-        "nvme_devices": _get_nvme_devices(),
-        "nvme_pcie_list": _get_nvme_pcie_list(),
-        "spdk_devices": _get_spdk_devices(),
-        "spdk_pcie_list": _get_spdk_pcie_list(),
+        "nvme_devices": node_utils._get_nvme_devices(),
+        "nvme_pcie_list": node_utils._get_nvme_pcie_list(),
+        "spdk_devices": node_utils._get_spdk_devices(),
+        "spdk_pcie_list": node_utils._get_spdk_pcie_list(),
     }
     return utils.get_response(out)
 
@@ -174,12 +87,11 @@ def spdk_process_start():
     spdk_image = constants.SIMPLY_BLOCK_SPDK_CORE_IMAGE
     if 'spdk_image' in data and data['spdk_image']:
         spdk_image = data['spdk_image']
-        # node_docker.images.pull(spdk_image)
 
-    # with open(spdk_deploy_yaml, 'r') as f:
-    #     dep = yaml.safe_load(f)
+    if _is_pod_up():
+        logger.info("SPDK deployment found, removing...")
+        spdk_process_kill()
 
-    from jinja2 import Environment, FileSystemLoader
     env = Environment(loader=FileSystemLoader(os.path.join(TOP_DIR, 'templates')), trim_blocks=True, lstrip_blocks=True)
     template = env.get_template('deploy_spdk.yaml.j2')
     values = {
@@ -252,91 +164,53 @@ def spdk_process_kill():
     return utils.get_response(True)
 
 
-@bp.route('/spdk_process_is_up', methods=['GET'])
-def spdk_process_is_up():
+def _is_pod_up():
     resp = k8s_core_v1.list_namespaced_pod(namespace)
     for pod in resp.items:
         if pod.metadata.name.startswith(pod_name):
             status = pod.status.phase
             if status == "Running":
-                return utils.get_response(True)
+                return True
             else:
-                return utils.get_response(False, f"SPDK container status: {status}")
-    return utils.get_response(False, "SPDK container not found")
+                return False
+    return False
 
 
-def _get_mem_info():
-    out, err, _ = run_command("cat /proc/meminfo")
-    data = {}
-    for line in out.split('\n'):
-        tm = line.split(":")
-        data[tm[0].strip()] = tm[1].strip()
-    return data
-
-
-def get_memory():
-    try:
-        mem_kb = _get_mem_info()['MemTotal']
-        mem_kb = mem_kb.replace(" ", "").lower()
-        mem_kb = mem_kb.replace("b", "")
-        return utils.parse_size(mem_kb)
-    except:
-        return 0
-
-
-def get_huge_memory():
-    try:
-        mem_kb = _get_mem_info()['Hugetlb']
-        mem_kb = mem_kb.replace(" ", "").lower()
-        mem_kb = mem_kb.replace("b", "")
-        return utils.parse_size(mem_kb)
-    except:
-        return 0
+@bp.route('/spdk_process_is_up', methods=['GET'])
+def spdk_process_is_up():
+    if _is_pod_up():
+        return utils.get_response(True)
+    else:
+        return utils.get_response(False, f"SPDK container is not running")
 
 
 @bp.route('/info', methods=['GET'])
 def get_info():
-    hostname, _, _ = run_command("hostname -s")
-    system_id, _, _ = run_command("dmidecode -s system-uuid")
 
     out = {
         "hostname": hostname,
         "system_id": system_id,
 
-        "cpu_count": cpuinfo.get_cpu_info()['count'],
-        "cpu_hz": cpuinfo.get_cpu_info()['hz_advertised'][0],
+        "cpu_count": cpu_info['count'],
+        "cpu_hz": cpu_info['hz_advertised'][0],
 
-        "memory": get_memory(),
-        "hugepages": get_huge_memory(),
+        "memory": node_utils.get_memory(),
+        "hugepages": node_utils.get_huge_memory(),
+        "memory_details": node_utils.get_memory_details(),
 
-        "nvme_devices": _get_nvme_devices(),
-        "nvme_pcie_list": _get_nvme_pcie_list(),
+        "nvme_devices": node_utils._get_nvme_devices(),
+        "nvme_pcie_list": node_utils._get_nvme_pcie_list(),
 
-        "spdk_devices": _get_spdk_devices(),
-        "spdk_pcie_list": _get_spdk_pcie_list(),
+        "spdk_devices": node_utils._get_spdk_devices(),
+        "spdk_pcie_list": node_utils._get_spdk_pcie_list(),
 
-        "network_interface": get_nics_data()
+        "network_interface": node_utils.get_nics_data()
     }
     return utils.get_response(out)
 
 
 @bp.route('/join_db', methods=['POST'])
 def join_db():
-    # data = request.get_json()
-    # db_connection = data['db_connection']
-    #
-    # logger.info("Setting DB connection")
-    # ret = scripts.set_db_config(db_connection)
-    #
-    # try:
-    #     node_docker = docker.DockerClient(base_url='unix://var/run/docker.sock')
-    #     nodes = node_docker.containers.list(all=True)
-    #     for node in nodes:
-    #         if node.attrs["Name"] == "/spdk_proxy":
-    #             node_docker.containers.get(node.attrs["Id"]).restart()
-    #             break
-    # except:
-    #     pass
     return utils.get_response(True)
 
 
@@ -348,7 +222,7 @@ def connect_to_nvme():
     nqn = data['nqn']
     st = f"nvme connect --transport=tcp --traddr={ip} --trsvcid={port} --nqn={nqn}"
     logger.debug(st)
-    out, err, ret_code = run_command(st)
+    out, err, ret_code = node_utils.run_command(st)
     logger.debug(ret_code)
     logger.debug(out)
     logger.debug(err)
@@ -363,7 +237,7 @@ def disconnect_device():
     data = request.get_json()
     dev_path = data['dev_path']
     st = f"nvme disconnect --device={dev_path}"
-    out, err, ret_code = run_command(st)
+    out, err, ret_code = node_utils.run_command(st)
     logger.debug(ret_code)
     logger.debug(out)
     logger.debug(err)
@@ -375,7 +249,7 @@ def disconnect_nqn():
     data = request.get_json()
     nqn = data['nqn']
     st = f"nvme disconnect --nqn={nqn}"
-    out, err, ret_code = run_command(st)
+    out, err, ret_code = node_utils.run_command(st)
     logger.debug(ret_code)
     logger.debug(out)
     logger.debug(err)
@@ -385,7 +259,7 @@ def disconnect_nqn():
 @bp.route('/disconnect_all', methods=['POST'])
 def disconnect_all():
     st = "nvme disconnect-all"
-    out, err, ret_code = run_command(st)
+    out, err, ret_code = node_utils.run_command(st)
     logger.debug(ret_code)
     logger.debug(out)
     logger.debug(err)
