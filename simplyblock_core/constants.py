ac8047262ad9c6e92322f6f0359d3d15d733edce
hamdy-simplyblock <hamdy@simplyblock.io>
2024-03-04 04:19:42 +0300

    set version: 1.0.4

--- simplyblock_core/constants.py
+++ simplyblock_core/constants.py
@@ -48,6 +48,6 @@ CACHING_NODE_MEMORY_FACTOR = 0.75
 HEALTH_CHECK_INTERVAL_SEC = 60
 
 
-SIMPLY_BLOCK_DOCKER_IMAGE = "simplyblock/simplyblock:v1.0.3"
+SIMPLY_BLOCK_DOCKER_IMAGE = "simplyblock/simplyblock:v1.0.4"
 SIMPLY_BLOCK_SPDK_CORE_IMAGE = "hamdykhader/spdk:core"
 SIMPLY_BLOCK_SPDK_ULTRA_IMAGE = "hamdykhader/spdk:latest"
