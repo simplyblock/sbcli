8344b46d8394b6379d9f81fa97905e3d8941fb72
hamdy-simplyblock <hamdy@simplyblock.io>
2024-04-04 16:02:25 +0300

    Use new spdk images
    set version: 2.0.8

--- simplyblock_core/constants.py
+++ simplyblock_core/constants.py
@@ -52,6 +52,6 @@ HEALTH_CHECK_INTERVAL_SEC = 60
 
 SIMPLY_BLOCK_DOCKER_IMAGE = "simplyblock/simplyblock:dev"
 SIMPLY_BLOCK_SPDK_CORE_IMAGE = "simplyblock/spdk-core:latest"
-SIMPLY_BLOCK_SPDK_ULTRA_IMAGE = "simplyblock/spdk:main"
+SIMPLY_BLOCK_SPDK_ULTRA_IMAGE = "simplyblock/spdk:main-latest"
 
 GELF_PORT = 12201
