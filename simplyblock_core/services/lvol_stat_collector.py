011e8258301cc270cb9fe01eb291c235bc86aafe
hamdy-simplyblock <hamdy@simplyblock.io>
2024-04-18 23:30:46 +0300

    Set LVol IO error to false if there is io stats for read and write
    set version: 2.2.6

--- simplyblock_core/services/lvol_stat_collector.py
+++ simplyblock_core/services/lvol_stat_collector.py
@@ -6,6 +6,7 @@ import sys
 
 
 from simplyblock_core import constants, kv_store, utils
+from simplyblock_core.controllers import lvol_events
 from simplyblock_core.models.stats import LVolStatObject, PoolStatObject
 from simplyblock_core.rpc_client import RPCClient
 
@@ -58,6 +59,13 @@ def add_lvol_stats(pool, lvol, stats_dict):
                 data['unmap_bytes_ps'] = int((data['unmap_bytes'] - last_record['unmap_bytes']) / time_diff)
                 data['unmap_io_ps'] = int((data['unmap_io'] - last_record['unmap_io']) / time_diff)
                 data['unmap_latency_ps'] = int((data['unmap_latency_ticks'] - last_record['unmap_latency_ticks']) / time_diff)
+
+                if data['read_io_ps'] > 0 and data['write_io_ps'] > 0 and lvol.io_error:
+                    # set lvol io error to false
+                    lvol = db_controller.get_lvol_by_id(lvol.get_id())
+                    lvol.io_error = False
+                    lvol.write_to_db(db_store)
+                    lvol_events.lvol_io_error_change(lvol, False, True, caused_by="monitor")
 
         else:
             logger.warning("last record not found")
