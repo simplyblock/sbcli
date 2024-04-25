eb651f305558235f738b2612f446ce45d6f2d925
geoffrey1330 <israelgeoffrey13@gmail.com>
2024-04-25 13:24:51 +0100

    Added logs and monitoring data cleanup

--- simplyblock_core/services/cleanup_graylog.py
+++ simplyblock_core/services/cleanup_graylog.py
@@ -1,8 +1,21 @@
 import subprocess
 import time
 import os
+import sys
+import logging
 
 from simplyblock_core import constants
+
+# Import the GELF logger
+from graypy import GELFUDPHandler
+
+logger_handler = logging.StreamHandler(stream=sys.stdout)
+logger_handler.setFormatter(logging.Formatter('%(asctime)s: %(levelname)s: %(message)s'))
+gelf_handler = GELFUDPHandler('0.0.0.0', constants.GELF_PORT)
+logger = logging.getLogger()
+logger.addHandler(gelf_handler)
+logger.addHandler(logger_handler)
+logger.setLevel(logging.DEBUG)
 
 deletion_interval = os.getenv('LOG_DELETION_INTERVAL', '10m')
 
@@ -21,10 +34,10 @@ while True:
     '''
     try:
         result = subprocess.run(delete_query, shell=True, check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        print("Logs deleted successfully.")
-        print(result.stdout.decode())
+        logger.info("Logs deleted successfully.")
+        logger.info(result.stdout.decode())
     except subprocess.CalledProcessError as e:
-        print("Failed to delete logs.")
-        print(e.stderr.decode())
+        logger.error("Failed to delete logs.")
+        logger.error(e.stderr.decode())
     
     time.sleep(constants.GRAYLOG_CHECK_INTERVAL_SEC)
