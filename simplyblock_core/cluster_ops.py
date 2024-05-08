bed7fc555f150922426cc114d7cd8a7e7671f6a9
geoffrey1330 <israelgeoffrey13@gmail.com>
2024-05-08 20:25:57 +0100

    added os.getenv('AWS_DEFAULT_REGION')

--- simplyblock_core/cluster_ops.py
+++ simplyblock_core/cluster_ops.py
@@ -18,7 +18,7 @@ from simplyblock_core.models.nvme_device import NVMeDevice
 from simplyblock_core.models.storage_node import StorageNode
 
 logger = logging.getLogger()
-region = os.getenv('AWS_DEFAULT_REGION')
+region = os.getenv('AWS_DEFAULT_REGION', 'us-east-1')
 ec2 = boto3.resource('ec2', region_name=region)
 
 def _add_graylog_input(cluster_ip, password):
