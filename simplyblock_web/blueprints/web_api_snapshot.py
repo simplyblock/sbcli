df94b6f0654c57a315439e9f039dd12a341d29ab
geoffrey1330 <israelgeoffrey13@gmail.com>
2024-05-17 14:08:56 +0100

    refactored web_api_snapshot.py code

--- simplyblock_web/blueprints/web_api_snapshot.py
+++ simplyblock_web/blueprints/web_api_snapshot.py
@@ -2,14 +2,13 @@
 # encoding: utf-8
 
 import logging
-from random import random
 
 from flask import Blueprint
 from flask import request
 
 from simplyblock_web import utils
 from simplyblock_core import kv_store
-from simplyblock_core.controllers import lvol_controller, snapshot_controller
+from simplyblock_core.controllers import snapshot_controller
 
 
 logger = logging.getLogger(__name__)
@@ -18,63 +17,27 @@ db_controller = kv_store.DBController()
 bp = Blueprint("snapshot", __name__)
 
 
-def get_param_value_or_default(data, key_name, default):
-    if key_name in data:
-        value = utils.parse_size(data[key_name])
-        if isinstance(value, int):
-            return value
-    return default
-
-
-def validate_header_data():
-    cl_id = request.headers.get('cluster')
-    secret = request.headers.get('secret')
-    logger.debug(f"Headers:cluster={cl_id}")
-    logger.debug(f"Headers:secret={secret}")
-    if not cl_id:
-        logger.error("no 'cluster' key found in the request headers")
-        return False
-    if not secret:
-        logger.error("no 'secret' key found in the request headers")
-        return False
-    cluster = db_controller.get_cluster_by_id(cl_id)
-    if not cluster:
-        logger.error(f"Cluster not found: {cl_id}")
-        return False
-    if cluster.secret == secret:
-        return True
-
-
 @bp.route('/snapshot/create_snapshot', methods=['POST'])
-def csi_create_snapshot():
-    if not validate_header_data():
-        return utils.get_csi_response(None, "Invalid Auth data, see API logs for more details.")
-
+def create_snapshot():
     cl_data = request.get_json()
     if 'lvol_id' not in cl_data:
-        return utils.get_csi_response(None, "missing required param: lvol_id", 400)
+        return utils.get_response(None, "missing required param: lvol_id", 400)
     if 'snapshot_name' not in cl_data:
-        return utils.get_csi_response(None, "missing required param: snapshot_name", 400)
+        return utils.get_response(None, "missing required param: snapshot_name", 400)
 
     snapID = snapshot_controller.add(
         cl_data['lvol_id'],
         cl_data['snapshot_name'])
-    return utils.get_csi_response(snapID)
+    return utils.get_response(snapID)
 
 
 @bp.route('/snapshot/delete_snapshot/<string:uuid>', methods=['DELETE'])
-def csi_delete_snapshot(uuid):
-    if not validate_header_data():
-        return utils.get_csi_response(None, "Invalid Auth data, see API logs for more details.")
-
+def delete_snapshot(uuid):
     ret = snapshot_controller.delete(uuid)
-    return utils.get_csi_response(ret)
+    return utils.get_response(ret)
 
 @bp.route('/snapshot/list_snapshots', methods=['GET'])
-def csi_list_snapshots():
-    if not validate_header_data():
-        return utils.get_csi_response(None, "Invalid Auth data, see API logs for more details.")
-
+def list_snapshots():
     snaps = db_controller.get_snapshots()
     data = []
     for snap in snaps:
@@ -88,4 +51,4 @@ def csi_list_snapshots():
             "source_uuid": snap.lvol.get_id(),
             "created_at": str(snap.created_at),
         })
-    return utils.get_csi_response(data)
+    return utils.get_response(data)
