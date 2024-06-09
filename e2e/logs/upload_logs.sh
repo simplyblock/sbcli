d7419805435db989f9c9cf3ce409f57e06211437
Raunak Jalan <ronakjalan98@gmail.com>
2024-06-09 17:23:48 +0530

    Modifying e2e yaml and adding test logs upload

---
+++ e2e/logs/upload_logs.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+sudo yum install -y unzip
+
+if [ ! -f "awscliv2.zip" ]; then
+    curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+    unzip awscliv2.zip
+    sudo ./aws/install
+else
+    echo "awscli already exists."
+fi
+
+# Configure AWS CLI with provided environment variables
+aws configure set aws_access_key_id $AWS_ACCESS_KEY_ID
+aws configure set aws_secret_access_key $AWS_SECRET_ACCESS_KEY
+aws configure set default.region $AWS_DEFAULT_REGION
+aws configure set default.output json
+
+LOCAL_LOGS_DIR="$RUN_ID"
+
+for file in *.log; do
+    if [ -f "$file" ]; then
+        filename=$(basename "$file")
+        aws s3 cp "$file" "s3://$S3_BUCKET/$LOCAL_LOGS_DIR/$filename"
+    fi
+done
+
+rm -rf "*.log"
+
+echo "Done uploading test log files to S3 with run ID: ${RUN_ID}"
