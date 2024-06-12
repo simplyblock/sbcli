dcf997dc164f6250d9ed929af69b1a0b65f90b93
hamdy-simplyblock <hamdy@simplyblock.io>
2024-06-13 00:36:45 +0300

    Add new event function 3
    set version:2.9.5

--- simplyblock_core/services/distr_event_collector.py
+++ simplyblock_core/services/distr_event_collector.py
@@ -145,8 +145,8 @@ while True:
     )
     num_of_events = constants.DISTR_EVENT_COLLECTOR_NUM_OF_EVENTS
     try:
-        events = client.distr_status_events_get()
-        # events = client.distr_status_events_discard_then_get(0, num_of_events)
+        # events = client.distr_status_events_get()
+        events = client.distr_status_events_discard_then_get(0, num_of_events)
         if not events:
             logger.error("Distr events empty")
             continue
@@ -162,10 +162,10 @@ while True:
             logger.info(f"Processing event: {eid}")
             process_event(eid)
 
-        # logger.info(f"Discarding events: {num_of_events}")
-        # events = client.distr_status_events_discard_then_get(num_of_events, 0)
+        logger.info(f"Discarding events: {num_of_events}")
+        events = client.distr_status_events_discard_then_get(num_of_events, 0)
 
     except Exception as e:
-        logger.error("Failed to get distr events")
+        logger.error("Failed to process distr events")
         logger.exception(e)
         continue
