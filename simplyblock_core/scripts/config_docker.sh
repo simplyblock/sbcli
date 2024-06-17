376bf57267afa4f746fd41958a621634eea87b48
geoffrey1330 <israelgeoffrey13@gmail.com>
2024-06-17 13:01:41 +0100

    increased sbcli-mig to version 1.0.127

--- simplyblock_core/scripts/config_docker.sh
+++ simplyblock_core/scripts/config_docker.sh
@@ -11,7 +11,6 @@ then
   set_config ExecStart "/usr/bin/dockerd --containerd=/run/containerd/containerd.sock -H tcp://${DEV_IP}:2375 -H unix:///var/run/docker.sock -H fd://" /usr/lib/systemd/system/docker.service
   sudo systemctl daemon-reload
   sudo systemctl restart docker
-  sudo systemctl restart rexray
   sleep 3
 fi
 
