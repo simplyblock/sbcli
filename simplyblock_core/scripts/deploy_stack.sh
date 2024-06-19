e642f7187398b84960b9969162c7f83ccc4d70cb
geoffrey1330 <israelgeoffrey13@gmail.com>
2024-06-19 21:40:55 +0100

    added conditional selection of docker volume

--- simplyblock_core/scripts/deploy_stack.sh
+++ simplyblock_core/scripts/deploy_stack.sh
@@ -26,6 +26,7 @@ INSTANCE_ID=$(curl -s http://169.254.169.254/latest/meta-data/instance-id)
 if [ -n "$INSTANCE_ID" ]; then
   echo "This is an AWS EC2 instance. Instance ID: $INSTANCE_ID"
   export USE_EFS="rexray/efs"
+fi
 
 docker stack deploy --compose-file="$DIR"/docker-compose-swarm-monitoring.yml monitoring
 
