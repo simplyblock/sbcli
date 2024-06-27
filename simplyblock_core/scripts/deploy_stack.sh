d6274088f14d1d8b4816a9593fc8c8303e8f6f3d
geoffrey1330 <israelgeoffrey13@gmail.com>
2024-06-27 21:00:59 +0100

    added efs tags to rexray driver on installation

--- simplyblock_core/scripts/deploy_stack.sh
+++ simplyblock_core/scripts/deploy_stack.sh
@@ -27,6 +27,7 @@ if [ -n "$INSTANCE_ID" ]
 then
   export USE_EFS="rexray/efs"
   export INSTANCE_ID=$INSTANCE_ID
+  docker plugin install rexray/efs --grant-all-permissions EFS_TAG=$INSTANCE_ID  REXRAY_PREEMPT=true
 fi
 
 docker stack deploy --compose-file="$DIR"/docker-compose-swarm-monitoring.yml monitoring
