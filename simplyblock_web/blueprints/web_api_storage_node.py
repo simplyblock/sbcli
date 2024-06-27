6fcadd5aeb79e7785e186c4b3107c0fb7c1e3ffb
Waleed Mousa <32266980+wmousa@users.noreply.github.com>
2024-06-27 14:56:14 +0300

    Specify lvol/snaps limit and provisioning limit per storage node (#81)
    In this change, user can specify --max-lvol, --max-snap, --max-prov,
    --number-of-devices (Optional, mandatory if the instance is
    not supported ec2 instance)
    parameters that determine the required small pool count, large pool
    count and required huge pages.

--- simplyblock_web/blueprints/web_api_storage_node.py
+++ simplyblock_web/blueprints/web_api_storage_node.py
@@ -163,9 +163,21 @@ def storage_node_add():
     if 'ifname' not in req_data:
         return utils.get_response_error("missing required param: ifname", 400)
 
+    if 'max_lvol' not in req_data:
+        return utils.get_response_error("missing required param: max_lvol", 400)
+
+    if 'max_snap' not in req_data:
+        return utils.get_response_error("missing required param: max_snap", 400)
+
+    if 'max_prov' not in req_data:
+        return utils.get_response_error("missing required param: max_prov", 400)
+
     cluster_id = req_data['cluster_id']
     node_ip = req_data['node_ip']
     ifname = req_data['ifname']
+    max_lvol = req_data['max_lvol']
+    max_snap = req_data['max_snap']
+    max_prov = req_data['max_prov']
 
     spdk_image = None
     if 'spdk_image' in req_data:
@@ -180,15 +192,10 @@ def storage_node_add():
         data_nics = req_data['data_nics']
         data_nics = data_nics.split(",")
 
-    spdk_mem = None
-    if 'spdk_mem' in req_data:
-        mem = req_data['spdk_mem']
-        spdk_mem = utils.parse_size(mem)
-        if spdk_mem < 1 * 1024 * 1024:
-            return utils.get_response_error(f"SPDK memory:{mem} must be larger than 1G", 400)
+
 
     out = storage_node_ops.add_node(
-        cluster_id, node_ip, ifname, data_nics, spdk_mem,
+        cluster_id, node_ip, ifname, data_nics, max_lvol, max_snap, max_prov,
         spdk_image=spdk_image, spdk_debug=spdk_debug)
 
     return utils.get_response(out)
