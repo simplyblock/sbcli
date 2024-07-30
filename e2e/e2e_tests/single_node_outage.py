005cb67bbe212602e2f00e1508f1db2d01130f04
hamdy-simplyblock <hamdy@simplyblock.io>
2024-07-30 23:06:14 +0300

    increase wait

--- e2e/e2e_tests/single_node_outage.py
+++ e2e/e2e_tests/single_node_outage.py
@@ -142,7 +142,7 @@ class TestSingleNodeOutage(TestClusterBase):
         self.sbcli_utils.restart_node(node_uuid=no_lvol_node_uuid)
 
         self.logger.info(f"Waiting for node to become online, {no_lvol_node_uuid}")
-        self.sbcli_utils.wait_for_storage_node_status(no_lvol_node_uuid, "online", timeout=120)
+        self.sbcli_utils.wait_for_storage_node_status(no_lvol_node_uuid, "online", timeout=180)
         sleep_n_sec(10)
 
         self.validations(node_uuid=no_lvol_node_uuid,
