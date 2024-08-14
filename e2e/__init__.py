c49652ce5c4741130f4b68fa660127c126816dc0
Raunak Jalan <ronakjalan98@gmail.com>
2024-08-14 19:37:15 +0530

    Adding snapshot and cloning e2e test scnario 4-5

--- e2e/__init__.py
+++ e2e/__init__.py
@@ -3,7 +3,9 @@ from e2e_tests.single_node_failure import TestSingleNodeFailure
 from e2e_tests.single_node_multi_fio_perf import (
     TestSingleNodeMultipleFioPerfValidation,
 )
+from e2e_tests.multi_lvol_run_fio import TestMultiLVOLRunFIO
 from e2e_tests.cloning_and_snapshot.multi_lvol_snapshot_fio import TestMultiLvolFio
+from e2e_tests.cloning_and_snapshot.single_lvol_multi_clone import TestSingleLvolMultipleCloneRunFIO
 
 
 
@@ -12,6 +14,8 @@ def get_all_tests():
         TestSingleNodeOutage,
         TestSingleNodeMultipleFioPerfValidation,
         # TestSingleNodeFailure,
-        # TestMultiLvolFio, - Enable when testing
+        # TestMultiLvolFio, - Enable when testing snapshot and cloning
+        # TestMultiLVOLRunFIO, - Enable when testing snapshot and cloning
+        # TestSingleLvolMultipleCloneRunFIO, - Enable when testing snapshot and cloning
     ]
     return tests
