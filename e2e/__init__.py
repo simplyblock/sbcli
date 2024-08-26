bd2d581ff7ed17782e3923b9a13c13c85a238bf2
Raunak Jalan <ronakjalan98@gmail.com>
2024-08-26 17:39:57 +0530

    Combing 4,5,6,7 test scenario for cloning and snapshot

--- e2e/__init__.py
+++ e2e/__init__.py
@@ -3,10 +3,11 @@ from e2e_tests.single_node_failure import TestSingleNodeFailure
 from e2e_tests.single_node_multi_fio_perf import (
     TestSingleNodeMultipleFioPerfValidation,
 )
+from e2e_tests.multi_lvol_run_fio import TestManyLvolSameNode
 from e2e_tests.batch_lvol_limit import TestBatchLVOLsLimit
 from e2e_tests.cloning_and_snapshot.multi_lvol_snapshot_fio import TestMultiLvolFio
 from e2e_tests.cloning_and_snapshot.lvol_batch_clone import TestSnapshotBatchCloneLVOLs
-
+from e2e_tests.cloning_and_snapshot.single_lvol_multi_clone import TestManyClonesFromSameSnapshot
 
 
 def get_all_tests():
@@ -15,10 +16,12 @@ def get_all_tests():
         TestSingleNodeMultipleFioPerfValidation,
         # TestSingleNodeFailure,
         # TestBatchLVOLsLimit,
+        # TestManyLvolSameNode
 
         # Enable when testing snapshot and cloning
 
         # TestMultiLvolFio,
         # TestSnapshotBatchCloneLVOLs
+        # TestManyClonesFromSameSnapshot
     ]
     return tests
