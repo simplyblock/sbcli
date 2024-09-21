d8cff091439a0ba5be359f8c0722d8918aebb4ea
hamdy-simplyblock <hamdy@simplyblock.io>
2024-09-21 05:19:17 +0300

    fix sfam-1292

--- simplyblock_core/controllers/tasks_controller.py
+++ simplyblock_core/controllers/tasks_controller.py
@@ -110,7 +110,7 @@ def get_active_node_restart_task(cluster_id, node_id):
     tasks = db_controller.get_job_tasks(cluster_id)
     for task in tasks:
         if task.function_name == JobSchedule.FN_NODE_RESTART and task.node_id == node_id:
-            if task.status == JobSchedule.STATUS_RUNNING and task.canceled is False:
+            if task.status != JobSchedule.STATUS_DONE and task.canceled is False:
                 return task.uuid
     return False
 
@@ -119,7 +119,7 @@ def get_active_dev_restart_task(cluster_id, device_id):
     tasks = db_controller.get_job_tasks(cluster_id)
     for task in tasks:
         if task.function_name == JobSchedule.FN_DEV_RESTART and task.device_id == device_id:
-            if task.status == JobSchedule.STATUS_RUNNING and task.canceled is False:
+            if task.status != JobSchedule.STATUS_DONE and task.canceled is False:
                 return task.uuid
     return False
 
@@ -129,7 +129,7 @@ def get_active_node_mig_task(cluster_id, node_id):
     for task in tasks:
         if task.function_name in [JobSchedule.FN_FAILED_DEV_MIG, JobSchedule.FN_DEV_MIG,
                                   JobSchedule.FN_NEW_DEV_MIG] and task.node_id == node_id:
-            if task.status == JobSchedule.STATUS_RUNNING and task.canceled is False:
+            if task.status != JobSchedule.STATUS_DONE and task.canceled is False:
                 return task.uuid
     return False
 
@@ -160,6 +160,6 @@ def get_active_node_task(cluster_id, node_id):
     tasks = db_controller.get_job_tasks(cluster_id)
     for task in tasks:
         if task.node_id == node_id:
-            if task.status == JobSchedule.STATUS_RUNNING and task.canceled is False:
+            if task.status != JobSchedule.STATUS_DONE and task.canceled is False:
                 return task.uuid
     return False
