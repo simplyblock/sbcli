4d693c659c409f472ba93923ba2c84db6a27a6b1
hamdy-simplyblock <hamdy@simplyblock.io>
2024-09-28 07:18:47 +0300

    add lvstore to health check 1

--- simplyblock_core/services/health_check_service.py
+++ simplyblock_core/services/health_check_service.py
@@ -6,8 +6,7 @@ from datetime import datetime
 from simplyblock_core.controllers import health_controller, storage_events, device_events
 from simplyblock_core.models.storage_node import StorageNode
 from simplyblock_core.rpc_client import RPCClient
-from simplyblock_core import constants, kv_store, utils
-
+from simplyblock_core import constants, kv_store, utils, distr_controller
 
 logger = utils.get_logger(__name__)
 
@@ -136,7 +135,48 @@ while True:
                             logger.info(f"Checking bdev: {remote_device.remote_bdev} ... not found")
                         node_remote_devices_check &= bool(ret)
 
-                health_check_status = is_node_online and node_devices_check and node_remote_devices_check
+                lvstore_check = True
+                if snode.lvstore and snode.lvstore_stack:
+                    distribs_list = []
+                    for bdev in snode.lvstore_stack:
+                        type = bdev['type']
+                        if type == "bdev_raid":
+                            distribs_list = bdev["distribs_list"]
+
+                    for distr in distribs_list:
+                        ret = rpc_client.get_bdevs(distr)
+                        if ret:
+                            logger.info(f"Checking distr bdev : {distr} ... ok")
+                            logger.info("Checking Distr map ...")
+                            ret = rpc_client.distr_get_cluster_map(distr)
+                            if not ret:
+                                logger.error("Failed to get cluster map")
+                                lvstore_check = False
+                            else:
+                                results, is_passed = distr_controller.parse_distr_cluster_map(ret)
+                                if results:
+                                    logger.info(utils.print_table(results))
+                                    logger.info(f"Checking Distr map ... {is_passed}")
+                                else:
+                                    logger.error("Failed to parse distr cluster map")
+                                lvstore_check &= is_passed
+                        else:
+                            logger.info(f"Checking distr bdev : {distr} ... not found")
+                            lvstore_check = False
+                    ret = rpc_client.get_bdevs(snode.raid)
+                    if ret:
+                        logger.info(f"Checking raid bdev: {snode.raid} ... ok")
+                    else:
+                        logger.info(f"Checking raid bdev: {snode.raid} ... not found")
+                        lvstore_check = False
+                    ret = rpc_client.bdev_lvol_get_lvstores(snode.lvstore)
+                    if ret:
+                        logger.info(f"Checking lvstore: {snode.lvstore} ... ok")
+                    else:
+                        logger.info(f"Checking lvstore: {snode.lvstore} ... not found")
+                        lvstore_check = False
+
+                health_check_status = is_node_online and node_devices_check and node_remote_devices_check and lvstore_check
             set_node_health_check(snode, health_check_status)
 
     time.sleep(constants.HEALTH_CHECK_INTERVAL_SEC)
