690ab596ea6867d66bdf2e59fe7d65deac086211
Raunak Jalan <ronakjalan98@gmail.com>
2024-10-03 11:54:42 +0530

    Adding HA JM case

--- e2e/__init__.py
+++ e2e/__init__.py
@@ -4,6 +4,7 @@ from e2e_tests.single_node_multi_fio_perf import (
     TestLvolFioNpcs0, TestLvolFioNpcs1, TestLvolFioNpcs2,
 )
 from e2e_tests.cloning_and_snapshot.multi_lvol_snapshot_fio import TestMultiLvolFio
+from e2e_tests.ha_journal.lvol_journal_device_node_restart import TestDeviceNodeRestart
 
 
 
@@ -14,6 +15,7 @@ def get_all_tests():
         TestLvolFioNpcs1,
         TestLvolFioNpcs2,
         TestSingleNodeFailure,
+        TestDeviceNodeRestart
         # TestMultiLvolFio, - Enable when testing
     ]
     return tests
