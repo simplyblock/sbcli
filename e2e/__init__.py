0686c3834f4c38b9c7a84b47fbda74e28c2940f2
Raunak Jalan <ronakjalan98@gmail.com>
2024-10-15 12:22:03 +0530

    Adding data migration test step1-2

--- e2e/__init__.py
+++ e2e/__init__.py
@@ -19,6 +19,7 @@ def get_all_tests(custom=True, k8s_test=False):
         TestLvolFioNpcs1,
         TestLvolFioNpcs2,
         TestSingleNodeFailure,
+        FioWorkloadTest,
         # TestManyLvolSameNode,
         # TestBatchLVOLsLimit,
 
