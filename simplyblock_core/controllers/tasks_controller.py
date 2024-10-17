5af5eb61c7f1da613061c706cbbf1a8d5eff7db6
hamdy-simplyblock <hamdy@simplyblock.io>
2024-10-17 17:14:40 +0300

    fix sfam-1413

--- simplyblock_core/controllers/tasks_controller.py
+++ simplyblock_core/controllers/tasks_controller.py
@@ -52,6 +52,11 @@ def _add_task(function_name, cluster_id, node_id, device_id,
         if task_id:
             logger.info(f"Task found, skip adding new task: {task_id}")
             return False
+    elif function_name == JobSchedule.FN_DEV_MIG:
+        task_id = get_device_mig_task(cluster_id, node_id, function_params['distr_name'])
+        if task_id:
+            logger.info(f"Task found, skip adding new task: {task_id}")
+            return False
 
     task_obj = JobSchedule()
     task_obj.uuid = str(uuid.uuid4())
@@ -72,6 +77,8 @@ def _add_task(function_name, cluster_id, node_id, device_id,
 def add_device_mig_task(device_id):
     device = db_controller.get_storage_devices(device_id)
     for node in db_controller.get_storage_nodes_by_cluster_id(device.cluster_id):
+        if not node.lvols:
+            continue
         for bdev in node.lvstore_stack:
             if bdev['type'] == "bdev_distr":
                 _add_task(JobSchedule.FN_DEV_MIG, device.cluster_id, node.get_id(), device.get_id(),
@@ -190,10 +197,21 @@ def get_active_node_task(cluster_id, node_id):
                 return task.uuid
     return False
 
+
 def get_new_device_mig_task(cluster_id, node_id, distr_name):
     tasks = db_controller.get_job_tasks(cluster_id)
     for task in tasks:
         if task.function_name == JobSchedule.FN_NEW_DEV_MIG and task.node_id == node_id:
+            if task.status != JobSchedule.STATUS_DONE and task.canceled is False \
+                    and "distr_name" in task.function_params and task.function_params["distr_name"] == distr_name:
+                return task.uuid
+    return False
+
+
+def get_device_mig_task(cluster_id, node_id, distr_name):
+    tasks = db_controller.get_job_tasks(cluster_id)
+    for task in tasks:
+        if task.function_name == JobSchedule.FN_DEV_MIG and task.node_id == node_id:
             if task.status != JobSchedule.STATUS_DONE and task.canceled is False \
                     and "distr_name" in task.function_params and task.function_params["distr_name"] == distr_name:
                 return task.uuid
