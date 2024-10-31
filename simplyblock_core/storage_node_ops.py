10c58a438a251e4d0bbfee835ec0583194e8ec23
hamdy-simplyblock <hamdy@simplyblock.io>
2024-10-31 15:14:40 +0300

    fix remote connection

--- simplyblock_core/storage_node_ops.py
+++ simplyblock_core/storage_node_ops.py
@@ -1217,8 +1217,6 @@ def add_node(cluster_id, node_ip, iface_name, data_nics_list,
         logger.info("Connecting to remote JMs")
         snode.remote_jm_devices = _connect_to_remote_jm_devs(snode)
 
-    logger.info("Setting node status to Active")
-    snode.status = StorageNode.STATUS_ONLINE
     snode.write_to_db(kv_store)
 
     # make other nodes connect to the new devices
@@ -1256,6 +1254,10 @@ def add_node(cluster_id, node_ip, iface_name, data_nics_list,
         node.write_to_db(kv_store)
         logger.info(f"connected to devices count: {len(node.remote_devices)}")
         time.sleep(3)
+
+    logger.info("Setting node status to Active")
+    snode.status = StorageNode.STATUS_ONLINE
+    snode.write_to_db(kv_store)
 
     if cluster.status != cluster.STATUS_ACTIVE:
         logger.warning(f"The cluster status is not active ({cluster.status}), adding the node without distribs and lvstore")
