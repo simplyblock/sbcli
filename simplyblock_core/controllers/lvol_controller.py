9eb6cb6baa218680334082468d6c7072ce0b2068
hamdy-simplyblock <hamdy@simplyblock.io>
2024-11-30 01:55:49 +0300

    wio

--- simplyblock_core/controllers/lvol_controller.py
+++ simplyblock_core/controllers/lvol_controller.py
@@ -356,7 +356,7 @@ def add_lvol_ha(name, size, host_id_or_name, ha_type, pool_id_or_name, use_comp,
     if cl.prov_cap_crit and cl.prov_cap_crit < cluster_size_prov_util:
         msg = f"Cluster provisioned cap critical would be, util: {cluster_size_prov_util}% of cluster util: {cl.prov_cap_crit}"
         logger.error(msg)
-        return False, msg
+        # return False, msg
 
     elif cl.prov_cap_warn and cl.prov_cap_warn < cluster_size_prov_util:
         logger.warning(f"Cluster provisioned cap warning, util: {cluster_size_prov_util}% of cluster util: {cl.prov_cap_warn}")
