9239629fc1b05825e25e2cc7dc19e0dd1322c90a
hamdy-simplyblock <hamdy@simplyblock.io>
2024-11-30 03:58:05 +0300

    fix lvol health

--- simplyblock_core/controllers/lvol_controller.py
+++ simplyblock_core/controllers/lvol_controller.py
@@ -481,15 +481,15 @@ def add_lvol_ha(name, size, host_id_or_name, ha_type, pool_id_or_name, use_comp,
             #        "num_md_pages_per_cluster_ratio": 1,
             #    }
             #},
-            {
-                "type": "bdev_lvol",
-                "name": lvol.lvol_bdev,
-                "params": {
-                    "name": lvol.lvol_bdev,
-                    "size_in_mib": int(lvol.size/(1000*1000)),
-                    "lvs_name": lvol.lvs_name
-                }
-            }
+            # {
+            #     "type": "bdev_lvol",
+            #     "name": lvol.lvol_bdev,
+            #     "params": {
+            #         "name": lvol.lvol_bdev,
+            #         "size_in_mib": int(lvol.size/(1000*1000)),
+            #         "lvs_name": lvol.lvs_name
+            #     }
+            # }
         ]
     )
 
