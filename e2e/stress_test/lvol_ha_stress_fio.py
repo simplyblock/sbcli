ca99b9158c31289a14b0b64cac82a7e295fe3827
Raunak Jalan <ronakjalan98@gmail.com>
2024-12-01 00:00:28 +0530

    Testing network downtime

--- e2e/stress_test/lvol_ha_stress_fio.py
+++ e2e/stress_test/lvol_ha_stress_fio.py
@@ -170,68 +170,72 @@ class TestLvolHACluster(FioWorkloadTest):
         self.logger.info("Running failure scenarios.")
         
         # Sce-1 Graceful shutdown and restart
-        self.logger.info("Graceful shutdown and restart.")
-        timestamp = int(datetime.now().timestamp())
-
-        self.sbcli_utils.suspend_node(node_uuid=self.lvol_node)
-        sleep_n_sec(10)
-        self.sbcli_utils.shutdown_node(node_uuid=self.lvol_node)
-        self.sbcli_utils.wait_for_storage_node_status(self.lvol_node,
-                                                      ["unreachable", "offline"],
-                                                      timeout=800)
-        sleep_n_sec(30)
-        self.sbcli_utils.restart_node(node_uuid=self.lvol_node)
-        self.sbcli_utils.wait_for_storage_node_status(self.lvol_node,
-                                                      "online",
-                                                      timeout=800)
-        
-        self.validate_checksums()
-
-        sleep_n_sec(30)
-
-        self.logger.info(f"Fetching migration tasks for cluster {self.cluster_id}.")
-
-        self.logger.info(f"Validating migration tasks for node {self.lvol_node}.")
-        self.validate_migration_for_node(timestamp, 5000, None)
-        sleep_n_sec(30)
-
-        # Sce-2 Container stop and restart
-        self.logger.info("Container stop and restart.")
-        timestamp = int(datetime.now().timestamp())
-        node_details = self.sbcli_utils.get_storage_node_details(self.lvol_node)
-        node_ip = node_details[0]["mgmt_ip"]
-        self.ssh_obj.stop_spdk_process(node_ip)
-        self.sbcli_utils.wait_for_storage_node_status(self.lvol_node,
-                                                      "online",
-                                                      timeout=800)
-        
-        self.validate_checksums()
-        
-        sleep_n_sec(30)
-
-        self.logger.info(f"Fetching migration tasks for cluster {self.cluster_id}.")
-
-        self.logger.info(f"Validating migration tasks for node {self.lvol_node}.")
-        self.validate_migration_for_node(timestamp, 5000, None)
-        sleep_n_sec(30)
-
-
-        # Sce-3 Network stop and restart
-        # self.logger.info("Network stop and restart.")
+        # self.logger.info("Graceful shutdown and restart.")
         # timestamp = int(datetime.now().timestamp())
-        # cmd = 'nohup sh -c "sudo ifdown eth0 && sleep 30 && sudo ifup eth0" &'
-        # self.ssh_obj.exec_command(self.node, command=cmd)
+
+        # self.sbcli_utils.suspend_node(node_uuid=self.lvol_node)
+        # sleep_n_sec(10)
+        # self.sbcli_utils.shutdown_node(node_uuid=self.lvol_node)
+        # self.sbcli_utils.wait_for_storage_node_status(self.lvol_node,
+        #                                               ["unreachable", "offline"],
+        #                                               timeout=800)
+        # sleep_n_sec(30)
+        # self.sbcli_utils.restart_node(node_uuid=self.lvol_node)
         # self.sbcli_utils.wait_for_storage_node_status(self.lvol_node,
         #                                               "online",
         #                                               timeout=800)
         
+        # self.validate_checksums()
+
+        # sleep_n_sec(30)
+
         # self.logger.info(f"Fetching migration tasks for cluster {self.cluster_id}.")
 
         # self.logger.info(f"Validating migration tasks for node {self.lvol_node}.")
         # self.validate_migration_for_node(timestamp, 5000, None)
         # sleep_n_sec(30)
 
+        # Sce-2 Container stop and restart
+        # self.logger.info("Container stop and restart.")
+        # timestamp = int(datetime.now().timestamp())
+        # node_details = self.sbcli_utils.get_storage_node_details(self.lvol_node)
+        # node_ip = node_details[0]["mgmt_ip"]
+        # self.ssh_obj.stop_spdk_process(node_ip)
+        # self.sbcli_utils.wait_for_storage_node_status(self.lvol_node,
+        #                                               "online",
+        #                                               timeout=800)
+        
         # self.validate_checksums()
+        
+        # sleep_n_sec(30)
+
+        # self.logger.info(f"Fetching migration tasks for cluster {self.cluster_id}.")
+
+        # self.logger.info(f"Validating migration tasks for node {self.lvol_node}.")
+        # self.validate_migration_for_node(timestamp, 5000, None)
+        # sleep_n_sec(30)
+
+
+        # Sce-3 Network stop and restart
+        self.logger.info("Network stop and restart.")
+        timestamp = int(datetime.now().timestamp())
+        cmd = 'nohup sh -c "sudo ifdown eth0 && sleep 30 && sudo ifup eth0" &'
+        self.ssh_obj.exec_command(self.node, command=cmd)
+        sleep_n_sec(100)
+        self.sbcli_utils.wait_for_storage_node_status(self.lvol_node,
+                                                      "online",
+                                                      timeout=800)
+        
+        self.logger.info(f"Fetching migration tasks for cluster {self.cluster_id}.")
+        output, _ = self.ssh_obj.exec_command(node=self.mgmt_nodes[0], 
+                                              command=f"{self.base_cmd} cluster list-tasks {self.cluster_id}")
+        self.logger.info(f"Data migration output: {output}")
+
+        # self.logger.info(f"Validating migration tasks for node {self.lvol_node}.")
+        # self.validate_migration_for_node(timestamp, 5000, None)
+        sleep_n_sec(30)
+
+        self.validate_checksums()
         
         self.logger.info("Completed failure scenarios.")
 
