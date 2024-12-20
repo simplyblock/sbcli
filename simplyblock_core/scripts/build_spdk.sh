ffa769c515fa7bdd26299d856658ab0a9b7c6fea
hamdy-simplyblock <hamdy@simplyblock.io>
2024-12-20 14:25:54 +0300

    fix sn deploy

--- simplyblock_core/scripts/build_spdk.sh
+++ simplyblock_core/scripts/build_spdk.sh
@@ -1,13 +1,16 @@
 #!/usr/bin/env bash
 
 
-if [[ "1" == "$(gh auth status > /dev/null ; echo $?)" ]]
-then
-  gh auth login
+if [ -d "/tmp/ultra" ]; then
+  echo "skip clone"
+else
+  if [[ "1" == "$(gh auth status > /dev/null ; echo $?)" ]]; then
+    gh auth login
+  fi
+  gh repo clone simplyblock-io/ultra /tmp/ultra
 fi
 
-gh repo clone simplyblock-io/ultra
-cd ultra/testing/distrib_integration/
+cd /tmo/ultra/testing/distrib_integration/
 sudo bash -x spdk_deploy.sh --force -c 0xE -m 4000 # -c the cpu mask for spdk, -m the hugepages memory
 
 systemctl status spdk
