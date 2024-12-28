c074eddbc7450acc55ab220982f872c0cbfcab1a
hamdy-simplyblock <hamdy@simplyblock.io>
2024-12-28 15:26:54 +0300

    dix sec node fail over 1

--- simplyblock_core/storage_node_ops.py
+++ simplyblock_core/storage_node_ops.py
@@ -2702,6 +2702,11 @@ def recreate_lvstore_on_sec(snode):
             remote_rpc_client.bdev_distrib_force_to_non_leader(node.jm_vuid)
             time.sleep(2)
 
+        if node.jm_vuid:
+            ret = rpc_client.jc_explicit_synchronization(node.jm_vuid)
+            logger.info(f"JM Sync res: {ret}")
+            time.sleep(1)
+
         for lvol in lvol_list:
             is_created, error = lvol_controller.recreate_lvol_on_node(
                 lvol, snode, 1, ana_state="inaccessible")
@@ -2728,7 +2733,6 @@ def recreate_lvstore_on_sec(snode):
             # remote_rpc_client.bdev_distrib_force_to_non_leader(node.jm_vuid)
             # time.sleep(2)
             time.sleep(5)
-
 
         for lvol in lvol_list:
             for iface in snode.data_nics:
