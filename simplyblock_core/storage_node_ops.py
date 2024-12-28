7c9ebf77d6fdb3fa926686ca147492339b3bc35a
hamdy-simplyblock <hamdy@simplyblock.io>
2024-12-28 07:47:15 +0300

    fix node fallback 4

--- simplyblock_core/storage_node_ops.py
+++ simplyblock_core/storage_node_ops.py
@@ -2736,6 +2736,11 @@ def recreate_lvstore(snode):
 
     ret = rpc_client.bdev_examine(snode.raid)
     ret = rpc_client.bdev_wait_for_examine()
+    time.sleep(1)
+    rpc_client.bdev_lvol_set_leader(False, lvs_name=snode.lvstore)
+    rpc_client.bdev_distrib_force_to_non_leader(snode.jm_vuid)
+
+    # time.sleep(2)
 
     sec_node = None
     lvol_list = db_controller.get_lvols_by_node_id(snode.get_id())
@@ -2751,22 +2756,16 @@ def recreate_lvstore(snode):
                             ret = sec_rpc_client.nvmf_subsystem_listener_set_ana_state(
                                 lvol.nqn, iface.ip4_address, "4420", False, "inaccessible")
 
-            time.sleep(1)
+            # time.sleep(2)
             sec_rpc_client.bdev_lvol_set_leader(False, lvs_name=snode.lvstore)
-            time.sleep(1)
             sec_rpc_client.bdev_distrib_force_to_non_leader(snode.jm_vuid)
-            time.sleep(1)
-
-    rpc_client.bdev_lvol_set_leader(False, lvs_name=snode.lvstore)
-    rpc_client.bdev_distrib_force_to_non_leader(snode.jm_vuid)
-
-    time.sleep(2)
+            time.sleep(2)
 
     if snode.jm_vuid:
         ret = rpc_client.jc_explicit_synchronization(snode.jm_vuid)
         logger.info(f"JM Sync res: {ret}")
 
-    time.sleep(2)
+    # time.sleep(1)
     for lvol in lvol_list:
         is_created, error = lvol_controller.recreate_lvol_on_node(lvol, snode)
         if error:
@@ -2779,7 +2778,7 @@ def recreate_lvstore(snode):
         lvol.write_to_db(db_controller.kv_store)
 
     if sec_node and sec_node.status == StorageNode.STATUS_ONLINE:
-        time.sleep(5)
+        time.sleep(3)
         sec_rpc_client = RPCClient(sec_node.mgmt_ip, sec_node.rpc_port, sec_node.rpc_username, sec_node.rpc_password, timeout=3, retry=2)
         for lvol in lvol_list:
             if lvol.ha_type == "ha":
