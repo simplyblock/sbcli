c8bf9d2d72dfb54da89b1c9335ef6a286f962289
hamdy-simplyblock <hamdy@simplyblock.io>
2025-01-01 03:17:22 +0300

    fix setup post install cmd

--- setup.py
+++ setup.py
@@ -2,22 +2,21 @@ import os
 
 from setuptools import setup, find_packages
 
-from pathlib import Path
-
 from setuptools.command.install import install as _install
 
 
 def _post_install():
     from subprocess import getstatusoutput
-    _, out = getstatusoutput('activate-global-python-argcomplete;env')
+    _, out = getstatusoutput('activate-global-python-argcomplete --user')
     print(out)
 
-    if "zsh" in os.environ.get("SHELL", ""):
-        path = f"{os.environ.get('HOME')}/.zshenv"
-    else:
-        path = f"{os.environ.get('HOME')}/.bash_completion"
-    if os.path.isfile(path):
-        _, out = getstatusoutput(f'source {path}')
+    if os.environ.get("SHELL") and os.environ.get("HOME"):
+        if "zsh" in os.environ.get("SHELL", ""):
+            path = f"{os.environ.get('HOME')}/.zshenv"
+        else:
+            path = f"{os.environ.get('HOME')}/.bash_completion"
+        if os.path.isfile(path):
+            _, out = getstatusoutput(f'source {path}')
 
 
 class install(_install):
