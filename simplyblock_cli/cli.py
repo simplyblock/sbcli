82e8835626eb27a32c667102d5d1e4bd2868731a
ananttr <anant@simplyblock.io>
2025-01-06 16:55:58 +0530

    changing default value

--- simplyblock_cli/cli.py
+++ simplyblock_cli/cli.py
@@ -301,9 +301,9 @@ class CLIWrapper:
         sub_command.add_argument("--inflight-io-threshold", help='The number of inflight IOs allowed before the IO queuing starts', type=int, default=4)
         sub_command.add_argument("--enable-qos", help='Enable qos bdev for storage nodes', action='store_true', dest='enable_qos')
         sub_command.add_argument("--strict-node-anti-affinity", help='Enable strict node anti affinity for storage nodes', action='store_true')
-        sub_command.add_argument("--scale-after-usage", help='scale cluster storage after percentage usage',required=False,default="70")
-        sub_command.add_argument("--scale-strategy", help='scale cluster with strategy INCREMENTAL,UNIFORM,ADDSNODE',choices=["INCREMENTAL","UNIFORM","ADDSNODE"],required=False,default="ADDSNODE")
-        sub_command.add_argument("--check-capacity-interval", help='how often to check cluster capacity(minutes)',required=False,default="1")
+        sub_command.add_argument("--scale-after-usage", help='scale cluster storage after percentage usage',default="70")
+        sub_command.add_argument("--scale-strategy", help='scale cluster with strategy INCREMENTAL,UNIFORM,ADDSNODE',choices=["INCREMENTAL","UNIFORM","ADDSNODE"],default="ADDSNODE")
+        sub_command.add_argument("--check-capacity-interval", help='how often to check cluster capacity(minutes)',default="5")
 
 
         # add cluster
