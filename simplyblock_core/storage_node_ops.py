4ee5f43c28c8010b149ceafdc71a906ba45979d4
geoffrey1330 <israelgeoffrey13@gmail.com>
2025-01-21 12:26:24 +0100

    sleep for 120 sec after ebs attach

--- simplyblock_core/storage_node_ops.py
+++ simplyblock_core/storage_node_ops.py
@@ -1619,7 +1619,7 @@ def restart_storage_node(
                     logger.error("Failed to attach volumes.")
                     return False
 
-                time.sleep(60)
+                time.sleep(120)
                 snode.cloud_instance_id = new_cloud_instance_id
         else:
             node_ip = None
