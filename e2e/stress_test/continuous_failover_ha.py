1e62eed81bad112f7748738a037075733d2df9e9
Raunak Jalan <ronakjalan98@gmail.com>
2025-02-01 19:34:15 +0530

    Modified outage

--- e2e/stress_test/continuous_failover_ha.py
+++ e2e/stress_test/continuous_failover_ha.py
@@ -47,8 +47,8 @@ class RandomFailoverTest(TestLvolHACluster):
         self.outage_end_time = None
         self.node_vs_lvol = {}
         self.test_name = "continuous_random_failover_ha"
-        # self.outage_types = ["partial_nw", "network_interrupt", "spdk_crash", "graceful_shutdown"]
-        self.outage_types = ["network_interrupt", "spdk_crash", "graceful_shutdown"]
+        # self.outage_types = ["partial_nw", "network_interrupt", "container_stop", "graceful_shutdown"]
+        self.outage_types = ["network_interrupt", "container_stop", "graceful_shutdown"]
         # self.outage_types = ["network_interrupt"]
         self.blocked_ports = None
         self.outage_log_file = os.path.join("logs", f"outage_log_{datetime.now().strftime('%Y%m%d_%H%M%S')}.log")
