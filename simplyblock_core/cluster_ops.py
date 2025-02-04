0af0fef23236ed124f4ed4f8b1d57049c63a078a
geoffrey1330 <israelgeoffrey13@gmail.com>
2025-02-04 16:30:00 +0100

    rebased branch

--- simplyblock_core/cluster_ops.py
+++ simplyblock_core/cluster_ops.py
@@ -108,10 +108,6 @@ def _add_graylog_input(cluster_ip, password):
     return response.status_code == 201
 
 
-
-
-    
-
 def create_cluster(blk_size, page_size_in_blocks, cli_pass,
                    cap_warn, cap_crit, prov_cap_warn, prov_cap_crit, ifname, log_del_interval, metrics_retention_period,
                    contact_point, grafana_endpoint, distr_ndcs, distr_npcs, distr_bs, distr_chunk_bs, ha_type,
@@ -1344,4 +1340,3 @@ $CMD -d cluster graceful-startup $cl  --clear-data
 $CMD -d cluster activate $cl
 
     """
-    
