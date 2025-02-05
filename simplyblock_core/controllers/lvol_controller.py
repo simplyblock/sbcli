fb207684ba8888608cf233bc71f072ea5058c6ed
geoffrey1330 <israelgeoffrey13@gmail.com>
2025-02-05 10:24:22 +0100

    added DBController to add_lvol

--- simplyblock_core/controllers/lvol_controller.py
+++ simplyblock_core/controllers/lvol_controller.py
@@ -304,6 +304,7 @@ def add_lvol_ha(name, size, host_id_or_name, ha_type, pool_id_or_name, use_comp,
                 uid=None, pvc_name=None, namespace=None):
 
     logger.info(f"Adding LVol: {name}")
+    db_controller = DBController()
     host_node = None
     if host_id_or_name:
         host_node = db_controller.get_storage_node_by_id(host_id_or_name)
