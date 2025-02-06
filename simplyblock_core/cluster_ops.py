50176972e59cb765c4c5862c88e8852849b5bb69
geoffrey1330 <israelgeoffrey13@gmail.com>
2025-02-06 13:25:53 +0100

    added configuration for max_result_window for opensearch

--- simplyblock_core/cluster_ops.py
+++ simplyblock_core/cluster_ops.py
@@ -122,6 +122,7 @@ def _set_max_result_window(cluster_ip, max_window=50000):
         logger.info("Settings updated for existing indices.")
     else:
         logger.error(f"Failed to update settings for existing indices: {response.text}")
+        return False
     
     url_template = f"http://{cluster_ip}:9200/_template/all_indices_template"
     payload_template = json.dumps({
@@ -133,8 +134,10 @@ def _set_max_result_window(cluster_ip, max_window=50000):
     response_template = requests.put(url_template, headers=headers, data=payload_template)
     if response_template.status_code == 200:
         logger.info("Template created for future indices.")
+        return True
     else:
         logger.error(f"Failed to create template for future indices: {response_template.text}")
+        return False
 
 def create_cluster(blk_size, page_size_in_blocks, cli_pass,
                    cap_warn, cap_crit, prov_cap_warn, prov_cap_crit, ifname, log_del_interval, metrics_retention_period,
