8c0cc2181320614d86ab6b0a94ca542150c6f8a6
hamdy-simplyblock <hamdy@simplyblock.io>
2025-02-22 02:36:02 +0300

    open port when blocked after 60 sec

--- simplyblock_core/controllers/health_controller.py
+++ simplyblock_core/controllers/health_controller.py
@@ -106,8 +106,8 @@ def _check_spdk_process_up(ip):
 def _check_port_on_node(snode, port_id):
     try:
         snode_api = SNodeClient(f"{snode.mgmt_ip}:5000", timeout=3, retry=2)
-        iptables_command_output = snode_api.get_firewall()
-        result = jc.parse('iptables', iptables_command_output.decode())
+        iptables_command_output, _ = snode_api.get_firewall()
+        result = jc.parse('iptables', iptables_command_output)
         for chain in result:
             if chain['chain'] in ["INPUT", "OUTPUT"]:
                 for rule in chain['rules']:
@@ -118,7 +118,7 @@ def _check_port_on_node(snode, port_id):
 
         return True
     except Exception as e:
-        logger.debug(e)
+        logger.error(e)
     return True
 
 
