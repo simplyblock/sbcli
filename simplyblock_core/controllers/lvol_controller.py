cbf8000234fb5e381e734cef6808c40e3e16d413
hamdy-simplyblock <hamdy@simplyblock.io>
2025-02-24 22:05:12 +0300

    do not create lvol when secondary is down

--- simplyblock_core/controllers/lvol_controller.py
+++ simplyblock_core/controllers/lvol_controller.py
@@ -494,18 +494,30 @@ def add_lvol_ha(name, size, host_id_or_name, ha_type, pool_id_or_name, use_comp,
     lvol.write_to_db(db_controller.kv_store)
 
     if host_node.status == StorageNode.STATUS_ONLINE:
+        if ha_type == "single":
+            lvol_bdev, error = add_lvol_on_node(lvol, host_node)
+            if error:
+                lvol.remove(db_controller.kv_store)
+                return False, error
+            lvol.nodes = [host_node.get_id()]
+            lvol.lvol_uuid = lvol_bdev['uuid']
+            lvol.blobid = lvol_bdev['driver_specific']['lvol']['blobid']
 
-        lvol_bdev, error = add_lvol_on_node(lvol, host_node)
-        if error:
-            lvol.remove(db_controller.kv_store)
-            return False, error
-        lvol.nodes = [host_node.get_id()]
-        lvol.lvol_uuid = lvol_bdev['uuid']
-        lvol.blobid = lvol_bdev['driver_specific']['lvol']['blobid']
-
-        if ha_type == "ha":
-            lvol.nodes.append(host_node.secondary_node_id)
+        else:
             sec_node = db_controller.get_storage_node_by_id(host_node.secondary_node_id)
+            if sec_node and sec_node.status == StorageNode.STATUS_DOWN:
+                msg = f"Secondary node is in down status, can not create lvol"
+                logger.error(msg)
+                lvol.remove(db_controller.kv_store)
+                return False, msg
+
+            lvol_bdev, error = add_lvol_on_node(lvol, host_node)
+            if error:
+                lvol.remove(db_controller.kv_store)
+                return False, error
+            lvol.lvol_uuid = lvol_bdev['uuid']
+            lvol.blobid = lvol_bdev['driver_specific']['lvol']['blobid']
+            lvol.nodes = [host_node.get_id(), host_node.secondary_node_id]
             if sec_node and sec_node.status == StorageNode.STATUS_ONLINE:
                 ret, error = add_lvol_on_node(lvol, sec_node, ha_inode_self=1)
                 if error:
@@ -526,6 +538,7 @@ def add_lvol_ha(name, size, host_id_or_name, ha_type, pool_id_or_name, use_comp,
         if not sec_node or sec_node.status != StorageNode.STATUS_ONLINE:
             msg = f"Host nodes are not online"
             logger.error(msg)
+            lvol.remove(db_controller.kv_store)
             return False, msg
 
         lvol_bdev, error = add_lvol_on_node(lvol, sec_node, ha_inode_self=1, sec_is_prime=True)
