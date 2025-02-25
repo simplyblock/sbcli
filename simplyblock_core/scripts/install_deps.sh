743d00854390cf7965ee2c1b5fe16503fb1d19f1
hamdy-simplyblock <hamdy@simplyblock.io>
2025-02-25 20:13:29 +0300

    remove entrypoint and disable ipv6 on restart

--- simplyblock_core/scripts/install_deps.sh
+++ simplyblock_core/scripts/install_deps.sh
@@ -17,19 +17,11 @@ sudo chmod 777 /etc/foundationdb
 sudo modprobe nvme-tcp
 sudo modprobe nbd
 
-sudo sysctl -w net.ipv6.conf.all.disable_ipv6=1
 
-# required for graylog
-sudo sysctl -w vm.max_map_count=262144
+echo \"net.ipv6.conf.all.disable_ipv6=1\" | sudo tee /etc/sysctl.d/disable_ipv6.conf
+echo \"vm.max_map_count=262144\" | sudo tee /etc/sysctl.d/disable_ipv6.conf
+sudo sysctl --system
 
-echo "net.core.rmem_max=16777216" >> /etc/sysctl.conf
-echo "net.core.rmem_default=87380" >> /etc/sysctl.conf
-echo "net.ipv4.tcp_rmem=4096 87380 16777216" >> /etc/sysctl.conf
-echo "net.core.somaxconn=1024" >> /etc/sysctl.conf
-echo "net.ipv4.tcp_max_syn_backlog=4096" >> /etc/sysctl.conf
-echo "net.ipv4.tcp_window_scaling=1" >> /etc/sysctl.conf
-echo "net.ipv4.tcp_retries2=8" >> /etc/sysctl.conf
-sysctl -p  # Apply changes immediately
 
 sudo mkdir -p /etc/simplyblock
 sudo chmod 777 /etc/simplyblock
