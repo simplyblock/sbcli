30f6c058a9f1b08bd46d17c5ef71a6f7866baf3a
hamdy-simplyblock <hamdy@simplyblock.io>
2025-02-28 01:44:37 +0300

    update 2

--- simplyblock_web/app.py
+++ simplyblock_web/app.py
@@ -2,22 +2,18 @@
 # encoding: utf-8
 
 import logging
-import sys
 from flask import Flask
 
 import utils
 from blueprints import web_api_cluster, web_api_mgmt_node, web_api_device, \
     web_api_lvol, web_api_storage_node, web_api_pool, web_api_caching_node, web_api_snapshot, web_api_deployer
 from auth_middleware import token_required
-from simplyblock_core import constants
+from simplyblock_core import constants, utils as core_utils
 from werkzeug.middleware.dispatcher import DispatcherMiddleware
 from prometheus_client import make_wsgi_app
 
-logger_handler = logging.StreamHandler(sys.stdout)
-logger_handler.setFormatter(logging.Formatter('%(asctime)s: %(levelname)s: %(message)s'))
-logger = logging.getLogger()
-logger.addHandler(logger_handler)
-logger.setLevel(constants.LOG_WEB_LEVEL)
+logger = core_utils.get_logger(__name__)
+
 
 
 app = Flask(__name__)
