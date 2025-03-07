44b0f677a07bbba7e8d736fa6fa1537dc38f2f49
hamdy-simplyblock <hamdy@simplyblock.io>
2025-03-07 05:58:13 +0300

    update spdk state collector service _1

--- simplyblock_core/services/spdk_stats_collector.py
+++ simplyblock_core/services/spdk_stats_collector.py
@@ -1,16 +1,19 @@
 import datetime
 import time
 
+import docker
+
+from simplyblock_core import utils
 from simplyblock_core.services.spdk import client as spdk_client
 
 
 method = "thread_get_stats"
-spdk_sock_path = '/var/tmp/spdk.sock'
-csv_file_path = "/etc/simplyblock/spdk_top.csv"
+logger = utils.get_logger(__name__)
 
-def get_file(ret):
 
-    f = open(csv_file_path, 'at', buffering=1)
+def get_file(ret, path):
+    logger.info(f"getting file {path}")
+    f = open(path, 'at', buffering=1)
     f.write("date, ")
     for stats in ret["threads"]:
         f.write(", ".join([k for k in stats]))
@@ -18,20 +21,35 @@ def get_file(ret):
     return f
 
 
+def get_docker_client():
+    return docker.from_env()
+
 if __name__ == '__main__':
 
-    client = spdk_client.JSONRPCClient(spdk_sock_path, 5260)
+    node_docker = get_docker_client()
 
-    out_file = None
     while True:
-        ret = client.call(method)
-        now = str(datetime.datetime.now()).split(".")[0]
-        if ret and "threads" in ret:
-            if not out_file:
-                out_file = get_file(ret)
-            out_file.write(now+", ")
-            for stats in ret["threads"]:
-                out_file.write(", ".join([str(k) for k in stats.values()]))
-            out_file.write("\n")
+        try:
+            for cont in node_docker.containers.list(all=True):
+                logger.info(cont.attrs['Name'])
+                if cont.attrs['Name'].startswith(f"/spdk") and not cont.attrs['Name'].startswith("/spdk_proxy") :
+                    status = cont.attrs['State']["Status"]
+                    is_running = cont.attrs['State']["Running"]
+                    logger.info(f"is_running: {is_running}")
+                    if is_running:
+                        spdk_sock_path = f"/var/tmp{cont.attrs['Name']}.sock"
+                        client = spdk_client.JSONRPCClient(spdk_sock_path, 5260)
+                        ret = client.call(method)
+                        now = str(datetime.datetime.now()).split(".")[0]
+                        if ret and "threads" in ret:
+                            out_file = get_file(ret, f"/etc/simplyblock{cont.attrs['Name']}_top.csv")
+                            out_file.write(now + ", ")
+                            for stats in ret["threads"]:
+                                out_file.write(", ".join([str(k) for k in stats.values()]))
+                            out_file.write("\n")
+
+        except Exception as e:
+            logger.error(e)
 
         time.sleep(10)
+
