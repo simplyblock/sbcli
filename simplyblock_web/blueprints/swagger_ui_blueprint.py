19a7bd33f7ee89d8f20964feff4f7601d75ae378
hamdy-simplyblock <hamdy@simplyblock.io>
2025-03-07 21:09:19 +0300

    add swagger _3

--- simplyblock_web/blueprints/swagger_ui_blueprint.py
+++ simplyblock_web/blueprints/swagger_ui_blueprint.py
@@ -1,12 +1,23 @@
+import os
+
+import yaml
 from flask_swagger_ui import get_swaggerui_blueprint
 
 SWAGGER_URL="/swagger"
-API_URL="/static/swagger.yaml"
+
+SCRIPT_PATH = os.path.dirname(os.path.realpath(__file__))
+API_URL=f"{SCRIPT_PATH}/../static/swagger.yaml"
+
+cnf = {}
+with open(API_URL) as f:
+    cnf = f.read()
+    cnf = yaml.load(cnf, Loader=yaml.SafeLoader)
 
 bp = get_swaggerui_blueprint(
     SWAGGER_URL,
     API_URL,
     config={
-        'app_name': 'Access API'
+        'app_name': 'Access API',
+        'spec': cnf,
     }
 )
