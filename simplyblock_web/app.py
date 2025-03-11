22260835be700a9ea482171e1549b87b84691181
hamdy-simplyblock <hamdy@simplyblock.io>
2025-03-11 07:21:47 +0300

    add sentry sdk _3

--- simplyblock_web/app.py
+++ simplyblock_web/app.py
@@ -22,6 +22,8 @@ sentry_sdk.init(
     # Add data like request headers and IP for users,
     # see https://docs.sentry.io/platforms/python/data-management/data-collected/ for more info
     send_default_pii=True,
+    traces_sample_rate=1.0,
+
 )
 
 app = Flask(__name__)
