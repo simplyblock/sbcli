396508c2fe6226078deead5887397d5b7db3fece
Alexander Sheredin <alexander@simplyblock.io>
2025-03-13 22:26:21 +0300

    fix 'lvol add'.

--- simplyblock_core/controllers/lvol_controller.py
+++ simplyblock_core/controllers/lvol_controller.py
@@ -404,7 +404,7 @@ def add_lvol_ha(name, size, host_id_or_name, ha_type, pool_id_or_name, use_comp,
     else:
         vuid = distr_vuid
 
-    if max_size:
+    if max_size > 0:
         if max_size < size:
             return False, f"Max size:{max_size} must be larger than size {size}"
     else:
