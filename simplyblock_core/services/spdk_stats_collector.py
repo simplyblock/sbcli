c79bba1b10c561a6ecb96caa12c6eea88b031eb4
geoffrey1330 <israelgeoffrey13@gmail.com>
2025-03-24 21:34:12 +0100

    added additional label to metrics

--- simplyblock_core/services/spdk_stats_collector.py
+++ simplyblock_core/services/spdk_stats_collector.py
@@ -1,6 +1,8 @@
 from prometheus_client import CollectorRegistry, Gauge, push_to_gateway
 import time
 from simplyblock_core.services.spdk import client as spdk_client
+from simplyblock_core import db_controller
+from simplyblock_core.rpc_client import RPCClient
 import socket
 import fcntl
 import struct
@@ -10,6 +12,8 @@ import subprocess
 PUSHGATEWAY_URL = "http://pushgateway:9091"
 SPDK_SOCK_PATH = "/var/tmp/spdk.sock"
 cluster_id_file = "/etc/foundationdb/sbcli_cluster_id"
+
+db_controller = db_controller.DBController()
 
 def get_node_ip(ifname):
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -30,22 +34,16 @@ def run_command(cmd):
     except Exception as e:
         return str(e)
 
-def get_cluster_id():
-    out = run_command(f"cat {cluster_id_file}")
-    return out
-
-def push_metrics(ret):
+def push_metrics(ret,cluster_id,snode_id):
     """Formats and pushes SPDK metrics to Prometheus Pushgateway."""
     registry = CollectorRegistry()
-    tick_rate_gauge = Gauge('tick_rate', 'SPDK Tick Rate', ['cluster_id','node_ip'], registry=registry)
-    cpu_busy_gauge = Gauge('cpu_busy_percentage', 'CPU Busy Percentage', ['cluster_id', 'node_ip', 'thread_name'], registry=registry)
-    pollers_count_gauge = Gauge('pollers_count', 'Number of pollers', ['cluster_id', 'node_ip', 'poller_type', 'thread_name'], registry=registry)
-
-    cluster_id = get_cluster_id()
+    tick_rate_gauge = Gauge('tick_rate', 'SPDK Tick Rate', ['cluster', 'snode', 'node_ip'], registry=registry)
+    cpu_busy_gauge = Gauge('cpu_busy_percentage', 'CPU Busy Percentage', ['cluster', 'snode', 'node_ip', 'thread_name'], registry=registry)
+    pollers_count_gauge = Gauge('pollers_count', 'Number of pollers', ['cluster', 'snode', 'node_ip', 'poller_type', 'thread_name'], registry=registry)
 
     tick_rate = ret.get("tick_rate")
     if tick_rate is not None:
-        tick_rate_gauge.labels(cluster_id=cluster_id, node_ip=NODE_IP).set(tick_rate)
+        tick_rate_gauge.labels(cluster=cluster_id, snode=snode_id, node_ip=NODE_IP).set(tick_rate)
     for thread in ret.get("threads", []):
         thread_name = thread.get("name")
         busy = thread.get("busy", 0)
@@ -53,22 +51,30 @@ def push_metrics(ret):
 
         total_cycles = busy + idle
         cpu_usage_percent = (busy / total_cycles) * 100 if total_cycles > 0 else 0
-        cpu_busy_gauge.labels(cluster_id=cluster_id, node_ip=NODE_IP, thread_name=thread_name).set(cpu_usage_percent)
+        cpu_busy_gauge.labels(cluster=cluster_id, snode=snode_id, node_ip=NODE_IP, thread_name=thread_name).set(cpu_usage_percent)
 
-        pollers_count_gauge.labels(cluster_id=cluster_id, node_ip=NODE_IP, poller_type="active", thread_name=thread_name).set(thread.get("active_pollers_count", 0))
-        pollers_count_gauge.labels(cluster_id=cluster_id, node_ip=NODE_IP, poller_type="timed", thread_name=thread_name).set(thread.get("timed_pollers_count", 0))
-        pollers_count_gauge.labels(cluster_id=cluster_id, node_ip=NODE_IP, poller_type="paused", thread_name=thread_name).set(thread.get("paused_pollers_count", 0))
+        pollers_count_gauge.labels(cluster=cluster_id, snode=snode_id, node_ip=NODE_IP, poller_type="active", thread_name=thread_name).set(thread.get("active_pollers_count", 0))
+        pollers_count_gauge.labels(cluster=cluster_id, snode=snode_id, node_ip=NODE_IP, poller_type="timed", thread_name=thread_name).set(thread.get("timed_pollers_count", 0))
+        pollers_count_gauge.labels(cluster=cluster_id, snode=snode_id, node_ip=NODE_IP, poller_type="paused", thread_name=thread_name).set(thread.get("paused_pollers_count", 0))
     
     push_to_gateway(PUSHGATEWAY_URL, job='metricsgateway', registry=registry)
     print("Metrics pushed successfully")
 
 if __name__ == "__main__":
-    client = spdk_client.JSONRPCClient(SPDK_SOCK_PATH, 5260)
     while True:
         try:
-            ret = client.call("thread_get_stats")
-            if ret and "threads" in ret:
-                push_metrics(ret)
+            clusters = db_controller.get_clusters()
+            for cluster in clusters:
+                cluster_id = cluster.get_id()
+                nodes = db_controller.get_storage_nodes_by_cluster_id(cluster_id)
+                for snode in nodes:
+                    rpc_client = RPCClient(
+                    snode.mgmt_ip, snode.rpc_port,
+                    snode.rpc_username, snode.rpc_password, timeout=3*60, retry=10)
+                    ret = rpc_client.thread_get_stats()
+                    print(f"spdk thread_get_stats return: {ret}")
+                    if ret and "threads" in ret:
+                        push_metrics(ret, cluster_id, snode.id)
         except Exception as e:
             print(f"SPDK query failed: {e}")
         
