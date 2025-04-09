26ad8af87a7bbdce52c4d20190fbe2b15930e027
Max Schettler <max@simplyblock.io>
2025-04-09 18:19:06 +0200

    Add basic node API documentation

--- simplyblock_web/blueprints/node_api_basic.py
+++ simplyblock_web/blueprints/node_api_basic.py
@@ -24,19 +24,24 @@ except:
     pass
 
 
-@api.get('/scan_devices', responses={
-    200: {'content': {'application/json': {'schema': utils.response_schema({
-        'type': 'object',
-        'required': ['nvme_devices', 'nvme_pcie_list', 'spdk_devices', 'spdk_pcie_list'],
-        'properties': {
-            'nvme_devices': {'type': 'array', 'items': {'type': 'string'}},
-            'nvme_pcie_list': {'type': 'array', 'items': {'type': 'string'}},
-            'spdk_devices': {'type': 'array', 'items': {'type': 'string'}},
-            'spdk_pcie_list': {'type': 'array', 'items': {'type': 'string'}},
-        },
-    })}}},
+@api.get('/scan_devices',
+    summary='Enumerate connected devices',
+    responses={
+        200: {'content': {'application/json': {'schema': utils.response_schema({
+            'type': 'object',
+            'required': ['nvme_devices', 'nvme_pcie_list', 'spdk_devices', 'spdk_pcie_list'],
+            'properties': {
+                'nvme_devices': {'type': 'array', 'items': {'type': 'string'}},
+                'nvme_pcie_list': {'type': 'array', 'items': {'type': 'string'}},
+                'spdk_devices': {'type': 'array', 'items': {'type': 'string'}},
+                'spdk_pcie_list': {'type': 'array', 'items': {'type': 'string'}},
+            },
+        })}},
+    },
 })
 def scan_devices():
+    """Query lists of connected NVMe and SPDK devices
+    """
     return utils.get_response({
         "nvme_devices": node_utils.get_nvme_devices(),
         "nvme_pcie_list": node_utils.get_nvme_pcie_list(),
@@ -45,14 +50,18 @@ def scan_devices():
     })
 
 
-@api.get('/info', responses={
-    200: {'content': {'application/json': {'schema': utils.response_schema({
-        'type': 'object',
-        'additionalProperties': True,
-    })}}},
+@api.get('/info',
+    summary='Get node info',
+    responses={
+        200: {'content': {'application/json': {'schema': utils.response_schema({
+            'type': 'object',
+            'additionalProperties': True,
+        })}},
+    },
 })
 def get_info():
-
+    """Retrieve information about the node's configuration and hardware
+    """
     out = {
         "hostname": hostname,
         "system_id": system_id,
@@ -81,12 +90,17 @@ class _NVMeParams(BaseModel):
     nqn: str
 
 
-@api.post('/nvme_connect', responses={
-    200: {'content': {'application/json': {'schema': utils.response_schema({
-        'type': 'boolean',
-    })}}},
+@api.post('/nvme_connect',
+    summary='Connect NVMe-oF target',
+    responses={
+        200: {'content': {'application/json': {'schema': utils.response_schema({
+            'type': 'boolean',
+        })}},
+    },
 })
 def connect_to_nvme(body: _NVMeParams):
+    """Connect to the indicated NVMe-oF target.
+    """
     st = f"nvme connect --transport=tcp --traddr={body.ip} --trsvcid={body.port} --nqn={body.nqn}"
     logger.debug(st)
     out, err, ret_code = shell_utils.run_command(st)
@@ -103,12 +117,17 @@ class _DisconnectParams(BaseModel):
     dev_path: str
 
 
-@api.post('/disconnect_device', responses={
-    200: {'content': {'application/json': {'schema': utils.response_schema({
-        'type': 'integer',
-    })}}},
+@api.post('/disconnect_device',
+    summary='Disconnect NVMe-oF device by name',
+    responses={
+        200: {'content': {'application/json': {'schema': utils.response_schema({
+            'type': 'integer',
+        })}},
+    },
 })
 def disconnect_device(body: _DisconnectParams):
+    """Disconnect from indicated NVMe-oF target
+    """
     st = f"nvme disconnect --device={body.dev_path}"
     out, err, ret_code = shell_utils.run_command(st)
     logger.debug(ret_code)
@@ -121,12 +140,16 @@ class _DisconnectNQNParams(BaseModel):
     nqn: str
 
 
-@api.post('/disconnect_nqn', responses={
+@api.post('/disconnect_nqn',
+    summary='Disconnect NVMe-oF device by NQN',
+    responses={
     200: {'content': {'application/json': {'schema': utils.response_schema({
         'type': 'integer',
     })}}},
 })
 def disconnect_nqn(body: _DisconnectNQNParams):
+    """Disconnect from indicated NVMe-oF target
+    """
     st = f"nvme disconnect --nqn={body.nqn}"
     out, err, ret_code = shell_utils.run_command(st)
     logger.debug(ret_code)
@@ -135,12 +158,17 @@ def disconnect_nqn(body: _DisconnectNQNParams):
     return utils.get_response(ret_code)
 
 
-@api.post('/disconnect_all', responses={
-    200: {'content': {'application/json': {'schema': utils.response_schema({
-        'type': 'integer',
-    })}}},
+@api.post('/disconnect_all',
+    summary='Disconnect all NVMe-oF devices',
+    responses={
+        200: {'content': {'application/json': {'schema': utils.response_schema({
+            'type': 'integer',
+        })}},
+    },
 })
 def disconnect_all():
+    """Disconnect from all NVMe-oF devices
+    """
     st = "nvme disconnect-all"
     out, err, ret_code = shell_utils.run_command(st)
     logger.debug(ret_code)
