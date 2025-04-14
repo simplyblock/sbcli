7cac156ffaf165cdb46fff9651cf7260b609b914
hamdykhader <hamdy.khader@gmail.com>
2025-04-14 16:17:28 +0300

    update lvol monitor

--- simplyblock_core/services/lvol_monitor.py
+++ simplyblock_core/services/lvol_monitor.py
@@ -83,17 +83,18 @@ while True:
                             sec_node.mgmt_ip, sec_node.rpc_port,
                             sec_node.rpc_username, sec_node.rpc_password, timeout=3, retry=2)
                         ret = sec_rpc_client.get_bdevs()
-                        for bdev in ret:
-                            sec_node_bdev_names[bdev['name']] = bdev
+                        if ret:
+                            for bdev in ret:
+                                sec_node_bdev_names[bdev['name']] = bdev
 
                         ret = sec_rpc_client.subsystem_list()
-                        for sub in ret:
-                            sec_node_lvols_nqns[sub['nqn']] = sub
+                        if ret:
+                            for sub in ret:
+                                sec_node_lvols_nqns[sub['nqn']] = sub
 
                 for lvol in db_controller.get_lvols_by_node_id(snode.get_id()):
 
-                    if lvol.io_error:
-                        logger.debug(f"Skipping LVol health check because of io_error {lvol.get_id()}")
+                    if lvol.status == LVol.STATUS_IN_CREATION:
                         continue
 
                     if lvol.status == lvol.STATUS_IN_DELETION:
@@ -116,12 +117,10 @@ while True:
                         continue
 
                     passed = True
-
-                    if lvol.ha_type == 'single':
-                        ret = health_controller.check_lvol_on_node(
-                            lvol.get_id(), lvol.node_id, node_bdev_names, node_lvols_nqns)
-                        if not ret:
-                            passed = False
+                    ret = health_controller.check_lvol_on_node(
+                        lvol.get_id(), lvol.node_id, node_bdev_names, node_lvols_nqns)
+                    if not ret:
+                        passed = False
 
 
                     if lvol.ha_type == "ha":
@@ -132,19 +131,20 @@ while True:
                             if not ret:
                                 passed = False
 
+                    if snode.lvstore_status == "ready":
 
-                    logger.info(f"LVol: {lvol.get_id()}, is healthy: {passed}")
-                    set_lvol_health_check(lvol, passed)
-                    if passed:
-                        set_lvol_status(lvol, LVol.STATUS_ONLINE)
+                        logger.info(f"LVol: {lvol.get_id()}, is healthy: {passed}")
+                        set_lvol_health_check(lvol, passed)
+                        if passed:
+                            set_lvol_status(lvol, LVol.STATUS_ONLINE)
 
-                for snap in db_controller.get_snapshots_by_node_id(snode.get_id()):
-                    if snap.snap_bdev in node_bdev_names:
-                        logger.info(f"Checking bdev: {snap.snap_bdev} ... ok")
-                        set_snapshot_health_check(snap, True)
-                    else:
-                        logger.error(f"Checking bdev: {snap.snap_bdev} ... failed")
-                        set_snapshot_health_check(snap, False)
-                        passed = False
+                        for snap in db_controller.get_snapshots_by_node_id(snode.get_id()):
+                            if snap.snap_bdev in node_bdev_names:
+                                logger.info(f"Checking bdev: {snap.snap_bdev} ... ok")
+                                set_snapshot_health_check(snap, True)
+                            else:
+                                logger.error(f"Checking bdev: {snap.snap_bdev} ... failed")
+                                set_snapshot_health_check(snap, False)
+                                passed = False
 
     time.sleep(constants.LVOL_MONITOR_INTERVAL_SEC)
