ce664b02a3e20a8b2686640c83bbc6b7ecb5a949
geoffrey1330 <israelgeoffrey13@gmail.com>
2025-05-02 11:54:11 +0100

    updated export variable value

--- simplyblock_core/scripts/deploy_stack.sh
+++ simplyblock_core/scripts/deploy_stack.sh
@@ -13,7 +13,7 @@ export LOG_DELETION_INTERVAL=$7
 export RETENTION_PERIOD=$8
 export LOG_LEVEL=$9
 export GRAFANA_ENDPOINT=${10}
-export FDB_DOCKER_IMAGE=$11
+export FDB_DOCKER_IMAGE=${11}
 
 export DIR="$(dirname "$(realpath "$0")")"
 
