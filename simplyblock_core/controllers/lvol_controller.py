991de876804d6b0320b65768470ece71f898f8e8
hamdykhader <hamdy.khader@gmail.com>
2025-05-03 07:37:41 +0300

    fix sfam-1953

--- simplyblock_core/controllers/lvol_controller.py
+++ simplyblock_core/controllers/lvol_controller.py
@@ -948,12 +948,7 @@ def delete_lvol(id_or_name, force_delete=False):
     if lvol.cloned_from_snap:
         snap = db_controller.get_snapshot_by_id(lvol.cloned_from_snap)
         if snap and snap.deleted is True:
-            lvols_count = 0
-            for lvol in db_controller.get_lvols():  # pass
-                if lvol.status != LVol.STATUS_IN_DELETION and lvol.cloned_from_snap == snap.get_id():
-                    lvols_count += 1
-            if lvols_count == 0:
-                snapshot_controller.delete(snap.get_id())
+            snapshot_controller.delete(snap.get_id())
 
     logger.info("Done")
     return True
