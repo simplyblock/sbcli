933508ed01f78174b9372ecb7fda57254c7e8d36
Max Schettler <max@simplyblock.io>
2025-05-07 11:25:11 +0200

    Ensure hublvol port is free

--- simplyblock_core/models/hublvol.py
+++ simplyblock_core/models/hublvol.py
@@ -9,7 +9,7 @@ class HubLVol(BaseModel):
     uuid: str = ""
     nqn: str = ""
     bdev_name: str = ""
-    nvmf_port: int = 9099
+    nvmf_port: int
     model_number: str = ""
     nguid: str = ""
 
