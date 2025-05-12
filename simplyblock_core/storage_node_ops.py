5a79c99acaf3881e8a147ffa6930ff759ddb476b
hamdykhader <hamdy.khader@gmail.com>
2025-05-12 19:23:35 +0300

    fix sfam-2012

--- simplyblock_core/storage_node_ops.py
+++ simplyblock_core/storage_node_ops.py
@@ -2973,7 +2973,6 @@ def recreate_lvstore(snode):
                 logger.error("Error establishing hublvol: %s", e)
                 # return False
             ### 8- allow secondary port
-            time.sleep(1)
             sec_node_api.firewall_set_port(snode.lvol_subsys_port, "tcp", "allow", sec_node.rpc_port)
             tcp_ports_events.port_allowed(sec_node, snode.lvol_subsys_port)
 
