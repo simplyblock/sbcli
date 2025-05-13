09734c746862fe13bcaa26aeabc39bfc57086583
Manohar Reddy <manohar@simplyblock.io>
2025-05-13 15:16:07 +0200

    rename function to get_config_var

--- simplyblock_core/constants.py
+++ simplyblock_core/constants.py
@@ -3,7 +3,7 @@ import os
 SCRIPT_PATH = os.path.dirname(os.path.realpath(__file__))
 
 
-def get_from_env_var_file(name, default=None):
+def get_config_var(name, default=None):
     """
     OS environment variable is checked first, if not found, check the env_var file.
     """
@@ -11,10 +11,11 @@ def get_from_env_var_file(name, default=None):
         return False
     if os.env.get(name):
         return os.env.get(name)
-    with open(f"{SCRIPT_PATH}/env_var", "r", encoding="utf-8") as fh:
-        for line in fh.readlines():
-            if line.startswith(name):
-                return line.split("=", 1)[1].strip()
+    else:
+        with open(f"{SCRIPT_PATH}/env_var", "r", encoding="utf-8") as fh:
+            for line in fh.readlines():
+                if line.startswith(name):
+                    return line.split("=", 1)[1].strip()
     return default
 
 
@@ -75,11 +76,11 @@ TASK_EXEC_INTERVAL_SEC = 10
 TASK_EXEC_RETRY_COUNT = 8
 
 SIMPLY_BLOCK_SPDK_CORE_IMAGE = "simplyblock/spdk-core:v24.05-tag-latest"
-SIMPLY_BLOCK_DOCKER_IMAGE = get_from_env_var_file(
+SIMPLY_BLOCK_DOCKER_IMAGE = get_config_var(
         "SIMPLY_BLOCK_DOCKER_IMAGE","simplyblock/simplyblock:main")
-SIMPLY_BLOCK_CLI_NAME = get_from_env_var_file(
+SIMPLY_BLOCK_CLI_NAME = get_config_var(
         "SIMPLY_BLOCK_COMMAND_NAME", "sbcli")
-SIMPLY_BLOCK_SPDK_ULTRA_IMAGE = get_from_env_var_file(
+SIMPLY_BLOCK_SPDK_ULTRA_IMAGE = get_config_var(
         "SIMPLY_BLOCK_SPDK_ULTRA_IMAGE", "public.ecr.aws/simply-block/ultra:main-latest")
 
 GELF_PORT = 12202
