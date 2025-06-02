69bdd15da1a80fa88421cf1a7519b084f04760e6
Raunak Jalan <41023976+RaunakJalan@users.noreply.github.com>
2025-06-02 21:10:19 +0530

    Raunak upgrade (#352)
    * Fix sbcli-dev
    
    * E2E-Adding sudo to commands
    
    * E2E-Adding sudo to commands
    
    * E2E-Removing sudo from redirect
    
    * E2E-Adding load test
    
    * Load-Testing load test on vm
    
    * Load-Testing load test on vm
    
    * E2E-Modifying upgrade path
    
    * E2E-Fixing lvol load
    
    * E2E-Fixing lvol load
    
    * E2E-Fixing upgrade steps
    
    * E2E-Adding lvols
    
    * E2E-Changing file path
    
    * E2E-Fix crash spdk

--- e2e/load.py
+++ e2e/load.py
@@ -88,6 +88,7 @@ parser = argparse.ArgumentParser(description="Run simplyBlock's Load Test Framew
 parser.add_argument('--testname', type=str, help="The name of the test to run", required=True)
 parser.add_argument('--output', type=str, help="Path to the log file", default='lvol_outage_log.csv')
 parser.add_argument('--max_lvols', type=int, help="Maximum number of lvols", default=1200)
+parser.add_argument('--start_lvols', type=int, help="Number of lvols to starrt outages", default=600)
 parser.add_argument('--step', type=int, help="Step count for lvols", default=100)
 parser.add_argument('--read_log', action='store_true', help="Only read log and generate graph")
 parser.add_argument('--continue_from_log', action='store_true', help="Continue from existing log")
@@ -116,20 +117,25 @@ try:
     test_obj = selected_test(
         output_file=args.output,
         max_lvols=args.max_lvols,
+        start_from=args.start_lvols,
         step=args.step,
         read_only=args.read_log,
         continue_from_log=args.continue_from_log
     )
+    test_obj.setup()
+    test_obj.cleanup_logs()
+    test_obj.configure_sysctl_settings()
     test_obj.run()
-    summary += f"✅ {selected_test.__name__}: PASSED\n"
+    summary += f"{selected_test.__name__}: PASSED\n"
     logger.info(f"Test {selected_test.__name__} completed successfully")
 except Exception as e:
     logger.error(traceback.format_exc())
-    summary += f"❌ {selected_test.__name__}: FAILED\n"
+    summary += f"{selected_test.__name__}: FAILED\n"
     if args.send_debug_notification or args.upload_logs:
         logger.error("Test failed. Logs and notification may still proceed.")
     raise MultipleExceptions({selected_test.__name__: [e]})
 finally:
+    test_obj.teardown()
     if check_for_dumps():
         logger.info("Found a core dump during test execution. Cluster is unstable.")
     if args.send_debug_notification:
--- e2e/load_tests/lvol_outage_load.py
+++ e2e/load_tests/lvol_outage_load.py
@@ -1,6 +1,7 @@
 import random
 import threading
 import csv
+from logger_config import setup_logger
 import matplotlib.pyplot as plt
 from datetime import datetime
 from pathlib import Path
@@ -14,24 +15,142 @@ class TestLvolOutageLoadTest(TestLvolHACluster):
     """
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-        self.output_file = Path(kwargs.get("output_file", "lvol_outage_log.csv"))
+        self.logger = setup_logger(__name__)
+        self.output_dir = Path("logs")
+        self.output_file = self.output_dir / kwargs.get("output_file", "lvol_outage_log.csv")
         self.max_lvols = kwargs.get("max_lvols", 1200)
         self.step = kwargs.get("step", 100)
         self.read_only = kwargs.get("read_only", False)
         self.continue_from_log = kwargs.get("continue_from_log", False)
-        self.start_from = 600
+        self.start_from = kwargs.get("start_from", 600)
+        self.lvol_size = "2G"
+        self.storage_nodes_uuid = []
+        self.lvol_node = None
+        self.fio_size = "250M"
+        self.mount_base = "/mnt/"
+        self.log_base = f"{Path.home()}/"
+        self.fio_threads = []
+        self.logger.info(f"Running load test with Max Lvol:{self.max_lvols}, Start: {self.start_from}, Step:{self.step}")
 
     def setup_environment(self):
-        self.node = self.mgmt_nodes[0]
+        storage_nodes = self.sbcli_utils.get_storage_nodes()
+        for result in storage_nodes['results']:
+            self.storage_nodes_uuid.append(result["uuid"])
+        self.lvol_node = random.choice(self.storage_nodes_uuid)
+
         self.sbcli_utils.add_storage_pool(pool_name=self.pool_name)
-        self.lvol_node = self.sbcli_utils.get_node_without_lvols()
-        self.ssh_obj.make_directory(node=self.node, dir_name=self.log_path)
+        base_lvol_name = "load_lvol"
+        for i in range(1, self.start_from + 1):
+            fs_type = random.choice(["ext4", "xfs"])
+            client_node = random.choice(self.fio_node)
+            lvol_name = f"{base_lvol_name}_{i}"
+            self.sbcli_utils.add_lvol(
+                lvol_name=lvol_name,
+                pool_name=self.pool_name,
+                size=self.lvol_size,
+                crypto=False,
+                key1=self.lvol_crypt_keys[0],
+                key2=self.lvol_crypt_keys[1],
+                host_id=self.lvol_node
+            )
+            sleep_n_sec(2)
+            lvol_id = self.sbcli_utils.get_lvol_id(lvol_name)
+            connect_ls = self.sbcli_utils.get_lvol_connect_str(lvol_name=lvol_name)
+            for cmd in connect_ls:
+                self.ssh_obj.exec_command(client_node, cmd)
+            device = self.ssh_obj.get_lvol_vs_device(client_node, lvol_id)
+            mount_path = f"{self.mount_base}/{lvol_name}"
+            self.lvol_mount_details[lvol_name] = {
+                   "ID": lvol_id,
+                   "Command": connect_ls,
+                   "Mount": mount_path,
+                   "Device": device,
+                   "MD5": None,
+                   "FS": fs_type,
+                   "Log": f"{self.log_base}/{lvol_name}.log",
+                   "snapshots": [],
+                   "Client": client_node
+            }
+            
+            self.ssh_obj.format_disk(client_node, device)
+            self.ssh_obj.mount_path(client_node, device, mount_path)
+
+            fio_thread = threading.Thread(
+                target=self.ssh_obj.run_fio_test,
+                args=(client_node, None, mount_path, self.lvol_mount_details[lvol_name]["Log"]),
+                kwargs={
+                    "size": self.fio_size,
+                    "name": f"{lvol_name}_fio",
+                    "rw": "randrw",
+                    "nrfiles": 5,
+                    "iodepth": 1,
+                    "numjobs": 6,
+                    "runtime": 500,
+                },
+            )
+            fio_thread.start()
+            self.fio_threads.append(fio_thread)
 
     def create_and_shutdown_restart(self, count):
-        self.total_lvols = count
-        self.create_lvols()
+        base_lvol_name = "load_lvol"
+        for i in range(count + 1, count + self.step + 1):
+            fs_type = random.choice(["ext4", "xfs"])
+            client_node = random.choice(self.fio_node)
+            lvol_name = f"{base_lvol_name}_{i}"
+            self.sbcli_utils.add_lvol(
+                lvol_name=lvol_name,
+                pool_name=self.pool_name,
+                size=self.lvol_size,
+                crypto=False,
+                key1=self.lvol_crypt_keys[0],
+                key2=self.lvol_crypt_keys[1],
+                host_id=self.lvol_node
+            )
+            sleep_n_sec(2)
+            lvol_id = self.sbcli_utils.get_lvol_id(lvol_name)
+            connect_ls = self.sbcli_utils.get_lvol_connect_str(lvol_name=lvol_name)
+            for cmd in connect_ls:
+                self.ssh_obj.exec_command(client_node, cmd)
+            device = self.ssh_obj.get_lvol_vs_device(client_node, lvol_id)
+            mount_path = f"{self.mount_base}/{lvol_name}"
+            self.lvol_mount_details[lvol_name] = {
+                   "ID": lvol_id,
+                   "Command": connect_ls,
+                   "Mount": mount_path,
+                   "Device": device,
+                   "MD5": None,
+                   "FS": fs_type,
+                   "Log": f"{self.log_base}/{lvol_name}.log",
+                   "snapshots": [],
+                   "Client": client_node
+            }
+            
+            self.ssh_obj.format_disk(client_node, device)
+            self.ssh_obj.mount_path(client_node, device, mount_path)
 
-        self.logger.info(f"[{count}] Initiating graceful shutdown...")
+            fio_thread = threading.Thread(
+                target=self.ssh_obj.run_fio_test,
+                args=(client_node, None, mount_path, self.lvol_mount_details[lvol_name]["Log"]),
+                kwargs={
+                    "size": self.fio_size,
+                    "name": f"{lvol_name}_fio",
+                    "rw": "randrw",
+                    "nrfiles": 5,
+                    "iodepth": 1,
+                    "numjobs": 6,
+                    "runtime": 500,
+                },
+            )
+            fio_thread.start()
+            self.fio_threads.append(fio_thread)
+        
+        count = count + self.step
+
+        self.common_utils.manage_fio_threads(
+            self.fio_node, self.fio_threads, timeout=1000
+        )
+
+        self.logger.info(f"[{count}] Initiating graceful shutdown {self.lvol_node}...")
         shutdown_start = datetime.now()
         self.sbcli_utils.suspend_node(node_uuid=self.lvol_node, expected_error_code=[503])
         self.sbcli_utils.wait_for_storage_node_status(self.lvol_node, "suspended", timeout=4000)
@@ -40,7 +159,7 @@ class TestLvolOutageLoadTest(TestLvolHACluster):
         self.sbcli_utils.wait_for_storage_node_status(self.lvol_node, "offline", timeout=4000)
         shutdown_end = datetime.now()
 
-        self.logger.info(f"[{count}] Shutdown complete, restarting node...")
+        self.logger.info(f"[{count}] Shutdown complete, restarting node {self.lvol_node}...")
         restart_start = datetime.now()
         self.sbcli_utils.restart_node(node_uuid=self.lvol_node, expected_error_code=[503])
         self.sbcli_utils.wait_for_storage_node_status(self.lvol_node, "online", timeout=4000)
@@ -50,7 +169,7 @@ class TestLvolOutageLoadTest(TestLvolHACluster):
         return shutdown_end - shutdown_start, restart_end - restart_start
 
     def write_to_log(self, lvol_count, shutdown_time, restart_time):
-        entry = [lvol_count, shutdown_time.total_seconds(), restart_time.total_seconds()]
+        entry = [lvol_count, shutdown_time.total_seconds() - 10, restart_time.total_seconds()]
         write_header = not self.output_file.exists()
         with open(self.output_file, 'a', newline='') as f:
             writer = csv.writer(f)
@@ -88,7 +207,7 @@ class TestLvolOutageLoadTest(TestLvolHACluster):
         plt.grid(True)
         plt.legend()
         plt.tight_layout()
-        plt.savefig("lvol_shutdown_restart_graph.png")
+        plt.savefig(self.output_dir / "lvol_shutdown_restart_graph.png")
         plt.show()
 
     def run(self):
@@ -102,7 +221,7 @@ class TestLvolOutageLoadTest(TestLvolHACluster):
         if self.continue_from_log:
             previous = self.parse_existing_log()
             if previous:
-                start = max([entry['lvol_count'] for entry in previous]) + self.step
+                start = max([entry['lvol_count'] for entry in previous])
 
         for count in range(start, self.max_lvols + 1, self.step):
             shutdown_time, restart_time = self.create_and_shutdown_restart(count)
