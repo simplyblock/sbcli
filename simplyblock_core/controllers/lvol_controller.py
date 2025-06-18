011e6d109b12871bc86d1720d205850b042e2d3c
Max Schettler <max@simplyblock.io>
2025-06-18 12:23:35 +0200

    Don't fail on empty iostat/capacity result

--- simplyblock_core/controllers/lvol_controller.py
+++ simplyblock_core/controllers/lvol_controller.py
@@ -1399,8 +1399,6 @@ def get_capacity(lvol_uuid, history, records_count=20, parse_sizes=True):
         records_number = 20
 
     records_list = db_controller.get_lvol_stats(lvol, limit=records_number)
-    if not records_list:
-        return False
     cap_stats_keys = [
         "date",
         "size_total",
@@ -1443,8 +1441,6 @@ def get_io_stats(lvol_uuid, history, records_count=20, parse_sizes=True, with_si
         records_number = 20
 
     records_list = db_controller.get_lvol_stats(lvol, limit=records_number)
-    if not records_list:
-        return False
     io_stats_keys = [
         "date",
         "read_bytes",
