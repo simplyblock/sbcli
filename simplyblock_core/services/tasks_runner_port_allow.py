simplyblock/sbcli

commit 998ddc84a769b06a9b5fc4dfb7ef8462021f73d6

fix SFAM-2093 _1

simplyblock_core/services/tasks_runner_port_allow.py
@@ -3,7 +3,7 @@ import time
 
 
 from simplyblock_core import db_controller, utils
-from simplyblock_core.controllers import tasks_events, tcp_ports_events
+from simplyblock_core.controllers import tasks_events, tcp_ports_events, health_controller
 from simplyblock_core.models.job_schedule import JobSchedule
 from simplyblock_core.models.cluster import Cluster
 from simplyblock_core.models.storage_node import StorageNode
@@ -57,8 +57,14 @@ while True:
                             task.write_to_db(db.kv_store)
                             continue
 
-                        if node.health_check is False:
-                            msg = f"Node health check fail: {node.get_id()}, retry later"
+                        lvstore_check = True
+                        if node.lvstore_status == "ready":
+                            lvstore_check &= health_controller._check_node_lvstore(node.lvstore_stack, node, auto_fix=True)
+                            if node.secondary_node_id:
+                                lvstore_check &= health_controller._check_node_hublvol(node)
+
+                        if lvstore_check is False:
+                            msg = f"Node LVolStore check fail, retry later"
                             logger.warning(msg)
                             task.function_result = msg
                             task.status = JobSchedule.STATUS_SUSPENDED

simplyblock_core/env_var
@@ -1,5 +1,5 @@
 SIMPLY_BLOCK_COMMAND_NAME=sbcli-dev
-SIMPLY_BLOCK_VERSION=19.0.3
+SIMPLY_BLOCK_VERSION=19.0.4
 
 
 SIMPLY_BLOCK_DOCKER_IMAGE=simplyblock/simplyblock:main

simplyblock_core/models/storage_node.py
@@ -236,12 +236,15 @@ class StorageNode(BaseNodeObject):
         remote_bdev = f"{primary_node.hublvol.bdev_name}n1"
 
         if not rpc_client.get_bdevs(remote_bdev):
-
+            ip_lst = []
             for ip in (iface.ip4_address for iface in primary_node.data_nics):
+                ip_lst.append(ip)
+            multipath = bool(len(ip_lst) > 1)
+            for ip in ip_lst:
                 ret = rpc_client.bdev_nvme_attach_controller_tcp(
                         primary_node.hublvol.bdev_name, primary_node.hublvol.nqn,
-                        ip, primary_node.hublvol.nvmf_port, multipath=True)
-                if not ret:
+                        ip, primary_node.hublvol.nvmf_port, multipath=multipath)
+                if not ret and not multipath:
                     logger.warning(f'Failed to connect to hublvol on {ip}')
 
         if not rpc_client.bdev_lvol_set_lvs_opts(

commit 7971067af66a8342d52838275ca3d4528a0a1bcb

Fix linter regression
This was introduced by 998ddc84a769b06a9b5fc4dfb7ef8462021f73d6.

simplyblock_core/services/tasks_runner_port_allow.py
@@ -64,7 +64,7 @@ while True:
                                 lvstore_check &= health_controller._check_node_hublvol(node)
 
                         if lvstore_check is False:
-                            msg = f"Node LVolStore check fail, retry later"
+                            msg = "Node LVolStore check fail, retry later"
                             logger.warning(msg)
                             task.function_result = msg
                             task.status = JobSchedule.STATUS_SUSPENDED