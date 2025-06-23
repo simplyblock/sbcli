0645df2da1b013aa51b471413dacebea630e26f1
geoffrey1330 <israelgeoffrey13@gmail.com>
2025-06-23 14:11:06 +0100

    add docs string to adjust_hugepages function

--- simplyblock_core/utils.py
+++ simplyblock_core/utils.py
@@ -1609,6 +1609,7 @@ def set_hugepages_if_needed(node, hugepages_needed, page_size_kb=2048):
         logger.error(f"Node {node}: Error occurred: {e}")
 
 def adjust_hugepages(hugepages):
+    """Adjust hugepages to the next multiple of 500 and add a small extra based on leading digits."""
     remainder = hugepages % 500
     hugepages =  hugepages + (500 - remainder)
 
