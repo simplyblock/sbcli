ce759b35c18061661300d53132cf02f0dee97cdc
Max Schettler <max@simplyblock.io>
2025-06-24 18:15:09 +0200

    Introduce bdev_lvol_block_data_port RPC call

--- simplyblock_core/rpc_client.py
+++ simplyblock_core/rpc_client.py
@@ -1083,3 +1083,8 @@ class RPCClient:
             "policy": policy,
         }
         return self._request("bdev_nvme_set_multipath_policy", params)
+
+    def bdev_lvol_block_data_port(self, lvs):
+        return self._request("bdev_lvol_block_data_port", {
+            "uuid" if utils.UUID_PATTERN.match(lvs) else "lvs_name": lvs,
+        })
