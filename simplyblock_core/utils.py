acdff09c2f954425216e5bc6630b14c81d765edb
schmidt-scaled <82834682+schmidt-scaled@users.noreply.github.com>
2025-06-26 13:19:41 +0100

    Update utils.py

--- simplyblock_core/utils.py
+++ simplyblock_core/utils.py
@@ -518,7 +518,7 @@ def calculate_minimum_hp_memory(small_pool_count, large_pool_count, lvol_count, 
     return: minimum_hp_memory in bytes
     '''
     pool_consumption = (small_pool_count * 8 + large_pool_count * 128) / 1024 + 1092
-    memory_consumption = (4 * cpu_count + 1.0277 * pool_consumption + 12 * lvol_count) * (1024 * 1024) + (
+    memory_consumption = (4 * cpu_count + 1.0277 * pool_consumption + 20 * lvol_count) * (1024 * 1024) + (
             250 * 1024 * 1024) * 1.1 * convert_size(max_prov, 'TiB') + constants.EXTRA_HUGE_PAGE_MEMORY
     return int(1.2*memory_consumption)
 
