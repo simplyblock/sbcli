c28e04bbf3489feaf28f4e1e1e8c6882ba82239f
Max Schettler <max@simplyblock.io>
2025-07-17 17:20:35 +0200

    Foo

--- simplyblock_cli/tests/test_cli.py
+++ simplyblock_cli/tests/test_cli.py
@@ -1,6 +1,6 @@
 from simplyblock_cli import cli
 
 def test_main_called(mocker):
-    mocker.patch('simplyblock_cli.cli.CLIWrapper')
+    mocker.patch.object('simplyblock_cli.cli.CLIWrapper')
     cli.main()
     cli.CLIWrapper.assert_called()
