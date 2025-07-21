8b60b8b0c94b6537c9747ec981124b4edda7f1a0
geoffrey1330 <israelgeoffrey13@gmail.com>
2025-07-21 15:32:55 +0100

    throw error when lvol count is greater

--- simplyblock_core/controllers/lvol_controller.py
+++ simplyblock_core/controllers/lvol_controller.py
@@ -290,7 +290,7 @@ def add_lvol_ha(name, size, host_id_or_name, ha_type, pool_id_or_name, use_comp,
             if lv.namespace == namespace:
                 lvols_count += 1
 
-        if lvols_count >= master_lvol.max_namespace_per_subsys:
+        if lvols_count > master_lvol.max_namespace_per_subsys:
             msg = f"Max namespaces reached: {lvols_count}"
             logger.error(msg)
             return False, msg
