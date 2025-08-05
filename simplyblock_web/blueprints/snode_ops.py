7eac55080e23243eafa241e1f932ff5fbe20a8fe
Geoffrey Israel <israelgeoffrey13@gmail.com>
2025-08-05 15:51:10 +0100

    added second check for enable_unsafe_noiommu_mode (#567)
    * added second check for enable_unsafe_noiommu_mode
    
    * use vfio-pci if device has an iommu group

--- simplyblock_web/blueprints/snode_ops.py
+++ simplyblock_web/blueprints/snode_ops.py
@@ -525,18 +525,27 @@ if not os.environ.get("WITHOUT_CLOUD_INFO"):
 
 @api.post('/bind_device_to_spdk')
 def bind_device_to_spdk(body: utils.DeviceParams):
-    if (
-            (vfio_module := Path('/sys/modules/vfio')).exists() and
-            pci_utils.driver_loaded(driver_name := 'vfio-pci')
-    ):
-        noiommu_parameter = vfio_module / 'parameters' / 'enable_unsafe_noiommu_mode'
-        if (
-                not (pci_utils.device(body.device_pci) / 'iommu_group').exists() and
-                noiommu_parameter.read_text().strip() == 'N'
-        ):
-            noiommu_parameter.write_text('1')
-    elif pci_utils.driver_loaded(driver_name := 'uio_pci_generic'):
-        pass  # assignment done in check
+    device_path = pci_utils.device(body.device_pci)
+    iommu_group = device_path / 'iommu_group'
+    vfio_module = Path('/sys/module/vfio')
+    noiommu_parameter = vfio_module / 'parameters' / 'enable_unsafe_noiommu_mode'
+    driver_name = None
+
+    if pci_utils.driver_loaded('vfio-pci'):
+        if iommu_group.exists():
+            driver_name = 'vfio-pci'
+        elif noiommu_parameter.exists():
+            if noiommu_parameter.read_text().strip() == 'N':
+                noiommu_parameter.write_text('1')
+            driver_name = 'vfio-pci'
+        elif pci_utils.driver_loaded('uio_pci_generic'):
+            driver_name = 'uio_pci_generic'
+        else:
+            return utils.get_response_error(
+                'SPDK PCI drivers are not fully loaded and device lacks IOMMU group', 500
+            )
+    elif pci_utils.driver_loaded('uio_pci_generic'):
+        driver_name = 'uio_pci_generic'
     else:
         return utils.get_response_error('SPDK PCI drivers are not loaded', 500)
 
