7593611fb67fa6c83419fd3411e8d027bcb025b4
michael <michael.schmidt1379@gmail.com>
2025-09-03 22:49:26 +0300

    connect settings update

--- simplyblock_core/rpc_client.py
+++ simplyblock_core/rpc_client.py
@@ -720,14 +720,14 @@ class RPCClient:
     def bdev_nvme_set_options(self):
         params = {
             # "action_on_timeout": "abort",
-            "bdev_retry_count": 1,
+            "bdev_retry_count": 0,
             "transport_retry_count": 3,
             "ctrlr_loss_timeout_sec": 1,
             "fast_io_fail_timeout_sec" : 0,
             "reconnect_delay_sec": 1,
-            "keep_alive_timeout_ms": 30000,
-            "timeout_us": constants.NVME_TIMEOUT_US,
-            "transport_ack_timeout": 13,
+            "keep_alive_timeout_ms": 10000,
+            "timeout_us": 4000000,
+            "transport_ack_timeout": 11,
         }
         return self._request("bdev_nvme_set_options", params)
 
