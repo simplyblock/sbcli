9b2afcc5f0ea8691b0c010698776044c7a9cf006
hamdykhader <hamdy.khader@gmail.com>
2025-09-17 18:48:38 +0300

    Fix SFAM-1465
    enable pool level qos settings

--- simplyblock_core/rpc_client.py
+++ simplyblock_core/rpc_client.py
@@ -615,23 +615,21 @@ class RPCClient:
         return self._request("bdev_set_qos_limit", params)
 
     def bdev_lvol_add_to_group(self, group_id, lvol_name_list):
-        return True
-        # params = {
-        #     "bdev_group_id": group_id ,
-        #     "lvol_vbdev_list": lvol_name_list
-        # }
-        # return self._request("bdev_lvol_add_to_group", params)
+        params = {
+            "bdev_group_id": group_id ,
+            "lvol_vbdev_list": lvol_name_list
+        }
+        return self._request("bdev_lvol_add_to_group", params)
 
     def bdev_lvol_set_qos_limit(self, bdev_group_id, rw_ios_per_sec, rw_mbytes_per_sec, r_mbytes_per_sec, w_mbytes_per_sec):
-        return True
-        # params = {
-        #     "bdev_group_id": bdev_group_id,
-        #     "rw_ios_per_sec": rw_ios_per_sec,
-        #     "rw_mbytes_per_sec": rw_mbytes_per_sec,
-        #     "r_mbytes_per_sec": r_mbytes_per_sec,
-        #     "w_mbytes_per_sec": w_mbytes_per_sec
-        # }
-        # return self._request("bdev_lvol_set_qos_limit", params)
+        params = {
+            "bdev_group_id": bdev_group_id,
+            "rw_ios_per_sec": rw_ios_per_sec,
+            "rw_mbytes_per_sec": rw_mbytes_per_sec,
+            "r_mbytes_per_sec": r_mbytes_per_sec,
+            "w_mbytes_per_sec": w_mbytes_per_sec
+        }
+        return self._request("bdev_lvol_set_qos_limit", params)
 
     def distr_send_cluster_map(self, params):
         return self._request("distr_send_cluster_map", params)
@@ -1176,3 +1174,10 @@ class RPCClient:
             "jm_vuid": jm_vuid,
         }
         return self._request("bdev_distrib_check_inflight_io", params)
+
+    def bdev_lvol_remove_from_group(self, group_id, lvol_name_list):
+        params = {
+            "bdev_group_id": group_id ,
+            "lvol_vbdev_list": lvol_name_list
+        }
+        return self._request("bdev_lvol_remove_from_group", params)
