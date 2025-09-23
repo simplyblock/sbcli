96559c9bfa7c360978998408fb4ce3a2ce7d6b74
hamdykhader <hamdy.khader@gmail.com>
2025-09-23 16:16:33 +0300

    wip

--- simplyblock_core/constants.py
+++ simplyblock_core/constants.py
@@ -215,3 +215,5 @@ prometheus_patch = {
     }
 }
 
+
+qos_class_weights = [60, 15, 0, 10 , 5,  0, 10, 0 ]
--- simplyblock_core/rpc_client.py
+++ simplyblock_core/rpc_client.py
@@ -1167,12 +1167,13 @@ class RPCClient:
         return self._request("jc_get_jm_status", params)
 
     def bdev_distrib_check_inflight_io(self, jm_vuid):
-        """
-        output: boolean value
-            'True': It means we have in-flight IOs in the target distrib group
-            'False': there is no in-flight IO
-        """
         params = {
             "jm_vuid": jm_vuid,
         }
         return self._request("bdev_distrib_check_inflight_io", params)
+
+    def alceml_set_qos_weights(self, qos_weights):
+        params = {
+            "qos_weights": qos_weights,
+        }
+        return self._request("alceml_set_qos_weights", params)
