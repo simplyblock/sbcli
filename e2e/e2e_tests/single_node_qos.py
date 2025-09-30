0b8cc53f615072eea21d077f986ca175351bbf09
RaunakJalan <ronakjalan98@gmail.com>
2025-09-30 23:32:05 +0530

    Adding qos e2e

---
+++ e2e/e2e_tests/single_node_qos.py
@@ -0,0 +1,280 @@
+from datetime import datetime
+import os
+from pathlib import Path
+from e2e_tests.cluster_test_base import TestClusterBase
+import threading
+import json
+from utils.common_utils import sleep_n_sec
+
+class TestLvolQOSBase(TestClusterBase):
+    """
+    Base class for handling common LVOL and FIO logic for different npcs configurations.
+    Inherits from TestClusterBase, which handles setup and teardown.
+    """
+
+    def setup(self):
+        """Call setup from TestClusterBase and then create the storage pool."""
+        self.test_name = "single_node_qos_perf"
+        
+        super().setup()
+
+        self.lvol_devices = {}
+
+        pools = self.sbcli_utils.list_storage_pools()
+        assert self.pool_name not in list(pools.keys()), \
+            f"Pool {self.pool_name} present in list of pools post delete: {pools}"
+        
+        assert f"{self.pool_name}_qos" not in list(pools.keys()), \
+            f"Pool {self.pool_name}_qos present in list of pools post delete: {pools}"
+
+        self.sbcli_utils.add_storage_pool(
+            pool_name=f"{self.pool_name}_qos",
+            cluster_id=self.cluster_id,
+            max_rw_iops=6000,
+            max_r_mbytes=90,
+            max_w_mbytes=90
+        )
+
+        self.sbcli_utils.add_storage_pool(
+            pool_name=self.pool_name,
+            cluster_id=self.cluster_id,
+        )
+
+        pools = self.sbcli_utils.list_storage_pools()
+        assert self.pool_name in list(pools.keys()), \
+            f"Pool {self.pool_name} not present in list of pools post add: {pools}"
+        
+        assert f"{self.pool_name}_qos" in list(pools.keys()), \
+            f"Pool {self.pool_name}_qos not present in list of pools post add: {pools}"
+
+    def create_lvols(self, lvol_configs, pool_qos=False):
+        """
+        Create multiple LVOLs, connect them, and mount them 
+        based on the provided configurations.
+        """
+        self.logger.info("Creating LVOLs based on the provided configurations")
+
+        for config in lvol_configs:
+            lvol_name = config['lvol_name']
+            if "ndcs" in lvol_configs:
+                self.sbcli_utils.add_lvol(
+                    lvol_name=lvol_name,
+                    pool_name=self.pool_name,
+                    size=config['size'],
+                    distr_ndcs=config['ndcs'],
+                    distr_npcs=config['npcs'],
+                    distr_bs=4096,
+                    distr_chunk_bs=4096,
+                )
+            else:
+                # Create LVOL
+                if pool_qos:
+                    self.sbcli_utils.add_lvol(
+                        lvol_name=lvol_name,
+                        pool_name=f"{self.pool_name}_qos",
+                        size=config['size'],
+                    )
+                else:
+                    self.sbcli_utils.add_lvol(
+                        lvol_name=lvol_name,
+                        pool_name=self.pool_name,
+                        size=config['size'],
+                        max_rw_iops=2000,
+                        max_r_mbytes=50,
+                        max_w_mbytes=50
+                    )
+
+
+            initial_devices = self.ssh_obj.get_devices(node=self.mgmt_nodes[0])
+
+            # Get LVOL connection string
+            connect_ls = self.sbcli_utils.get_lvol_connect_str(lvol_name=lvol_name)
+            for connect_str in connect_ls:
+                self.ssh_obj.exec_command(node=self.mgmt_nodes[0], command=connect_str)
+
+            # Identify the newly connected device
+            sleep_n_sec(10)
+            final_devices = self.ssh_obj.get_devices(node=self.mgmt_nodes[0])
+            disk_use = None
+
+            for device in final_devices:
+                if device not in initial_devices:
+                    self.logger.info(f"Using disk: /dev/{device.strip()}")
+                    disk_use = f"/dev/{device.strip()}"
+                    break
+
+            # Unmount, format, and mount the device
+            self.ssh_obj.unmount_path(node=self.mgmt_nodes[0], device=disk_use)
+            mount_path = None
+            if config["mount"]:
+                self.ssh_obj.format_disk(node=self.mgmt_nodes[0], device=disk_use)
+                mount_path = f"{Path.home()}/test_location_{lvol_name}"
+                self.ssh_obj.mount_path(node=self.mgmt_nodes[0], device=disk_use, mount_path=mount_path)
+
+            # Store device information
+            self.lvol_devices[lvol_name] = {"Device": disk_use, "MountPath": mount_path}
+
+    def run_fio_on_lvol(self, lvol_name, mount_path=None, device=None, readwrite="randrw"):
+        """Run FIO tests on a specific LVOL with the given readwrite operation."""
+        self.logger.info(f"Starting FIO test on {lvol_name} with readwrite={readwrite}")
+        fio_thread = threading.Thread(
+            target=self.ssh_obj.run_fio_test,
+            args=(self.fio_node[0], device, mount_path, None),
+            kwargs={
+                "name": f"fio_{lvol_name}",
+                "rw": readwrite,
+                "ioengine": "libaio",
+                "iodepth": 1,
+                "bs": 4096,
+                "size": "2G",
+                "time_based": True,
+                "runtime": 100,
+                "output_format": "json",
+                "output_file": f"{Path.home()}/{lvol_name}_log.json",
+                "nrfiles": 5,
+                "debug": self.fio_debug
+            }
+        )
+        fio_thread.start()
+        return fio_thread
+
+    def validate_fio_output(self, pool_qos_lvols_config, lvol_qos_config):
+        """Validate the FIO output for IOPS and MB/s."""
+        total_qos_iops = 0
+        total_qos_read_bw = 0
+        total_qos_write_bw = 0
+
+        for lvol_configs in [pool_qos_lvols_config, lvol_qos_config]:
+            for config in lvol_configs:
+                lvol_name = config["lvol_name"]
+                log_file = f"{Path.home()}/{lvol_name}_log.json"
+                output = self.ssh_obj.read_file(node=self.fio_node[0], file_name=log_file)
+                fio_result = ""
+                self.logger.info(f"FIO output for {lvol_name}: {output}")
+
+                start_index = output.find('{')  # Find the first opening brace
+                if start_index != -1:
+                    json_content = output[start_index:]  # Extract everything starting from the JSON
+                    try:
+                        self.logger.info(f"Removed str FIO output for {lvol_name}: {fio_result}")
+                        # Parse the extracted JSON
+                        fio_result = json.loads(json_content)
+                    except json.JSONDecodeError as e:
+                        print(f"Error decoding JSON: {e}")
+                        return None
+                else:
+                    print("No JSON content found in the file.")
+                    return None
+                # fio_result = json.loads(output)
+                self.logger.info(f"FIO output for {lvol_name}: {fio_result}")
+
+                job = fio_result['jobs'][0]
+                job_name = job['job options']['name']
+                file_name = job['job options'].get("directory", job['job options'].get("filename", None))
+                read_iops = job['read']['iops']
+                write_iops = job['write']['iops']
+                total_iops = read_iops + write_iops
+                disk_name = fio_result['disk_util'][0]['name']
+
+                read_bw_kb = job['read']['bw']
+                write_bw_kb = job['write']['bw']
+                trim_bw_kb = job['trim']['bw']
+                read_bw_mib = read_bw_kb / 1024
+                write_bw_mib = write_bw_kb / 1024
+                trim_bw_mib = trim_bw_kb / 1024
+                total_qos_iops += total_iops
+                total_qos_read_bw += read_bw_mib
+                total_qos_write_bw += write_bw_mib
+
+                # Write LVOL details to the text file
+                with open(os.path.join("logs" ,"fio_test_results.log"),
+                          "a", encoding="utf-8") as log_file:
+                    log_file.write(f"LVOL: {lvol_name}, Total IOPS: {total_iops}, Read BW: "
+                                   f"{read_bw_mib} MiB/s, Write BW: {write_bw_mib} MiB/s, "
+                                   f"Trim BW: {trim_bw_mib} MiB/s\n\n")
+                    
+
+            self.logger.info(f"Performing validation for FIO job: {job_name} on device: "
+                            f"{disk_name} mounted on: {file_name}")
+            
+        with open(os.path.join("logs" ,"fio_test_results.log"),
+                  "a", encoding="utf-8") as log_file:
+            log_file.write(f"Total QOS IOPS: {total_qos_iops}, Read BW: "
+                           f"{total_qos_read_bw} MiB/s, Write BW: {total_qos_write_bw} MiB/s")
+
+        assert  10000 < total_qos_iops < 12300 , \
+            f"Total IOPS {total_qos_iops} can not be more than 12300, should not be less than 10000"
+
+        assert 200 < read_bw_mib < 250, f"Read BW {total_qos_read_bw} out of range (200-250 MiB/s)"
+        assert 200 < write_bw_mib < 250, f"Write BW {total_qos_write_bw} out of range (200-250 MiB/s)"
+
+    def cleanup_lvols(self, lvol_configs):
+        """Unmount, remove directory, and delete LVOLs for cleanup."""
+        self.logger.info("Starting cleanup of LVOLs")
+        for config in lvol_configs:
+            lvol_name = config['lvol_name']
+            self.ssh_obj.unmount_path(node=self.mgmt_nodes[0], 
+                                      device=self.lvol_devices[lvol_name]['MountPath'])
+            self.ssh_obj.remove_dir(node=self.mgmt_nodes[0], 
+                                    dir_path=self.lvol_devices[lvol_name]['MountPath'])
+            lvol_id = self.sbcli_utils.get_lvol_id(lvol_name=lvol_name)
+            subsystems = self.ssh_obj.get_nvme_subsystems(node=self.mgmt_nodes[0], 
+                                                          nqn_filter=lvol_id)
+            for subsys in subsystems:
+                self.logger.info(f"Disconnecting NVMe subsystem: {subsys}")
+                self.ssh_obj.disconnect_nvme(node=self.mgmt_nodes[0], nqn_grep=subsys)
+            self.sbcli_utils.delete_lvol(lvol_name=lvol_name)
+        self.logger.info("Cleanup completed")
+
+
+class TestLvolFioQOSCombined(TestLvolQOSBase):
+    """
+    Test class for LVOLs with QOS setting from pool level and lvol level
+    """
+
+    def run(self):
+        """Custom test scenario without requiring ndcs or npcs."""
+        # Define custom test configurations that don't require ndcs or npcs
+        pool_qos_lvol_configs = [
+            {"lvol_name": "lvol_qos1", "size": "4G", "mount": True},
+            {"lvol_name": "lvol_qos2", "size": "4G", "mount": True},
+            {"lvol_name": "lvol_qos3", "size": "4G", "mount": True},
+        ]
+
+        lvol_configs = [
+            {"lvol_name": "lvol_non_qos1", "size": "4G", "mount": True},
+            {"lvol_name": "lvol_non_qos2", "size": "4G", "mount": True},
+            {"lvol_name": "lvol_non_qos3", "size": "4G", "mount": True},
+        ]
+        
+        # Create LVOLs
+        self.create_lvols(pool_qos_lvol_configs, pool_qos=True)
+        self.create_lvols(lvol_configs)
+
+        # Run FIO tests
+        fio_threads = []
+        for lvol_config in pool_qos_lvol_configs:
+            fio_threads.append(self.run_fio_on_lvol(lvol_config["lvol_name"],
+                                                    mount_path=self.lvol_devices[lvol_config["lvol_name"]]["MountPath"],
+                                                    readwrite="randrw"))
+        for lvol_config in lvol_configs:
+            fio_threads.append(self.run_fio_on_lvol(lvol_config["lvol_name"],
+                                                    mount_path=self.lvol_devices[lvol_config["lvol_name"]]["MountPath"],
+                                                    readwrite="randrw"))
+
+        self.common_utils.manage_fio_threads(
+            node=self.fio_node[0], threads=fio_threads, timeout=600
+        )
+
+        for thread in fio_threads:
+            thread.join()
+
+        # Validate FIO outputs
+        self.validate_fio_output(pool_qos_lvol_configs, lvol_configs)
+
+        # Cleanup after running FIO
+        self.cleanup_lvols(lvol_configs)
+        self.cleanup_lvols(pool_qos_lvol_configs)
+
+        self.logger.info("Test Case Passed.")
+
