1e3479ca46e730b7cdb15f79c8491ce6f7b043a8
Hamdy H. Khader <hamdy.khader@gmail.com>
2025-10-06 08:46:14 +0300

    Main sfam 2349 (Alceml qos classes) (#642)
    * wip
    
    * Implement SFAM-2349 1
    
    * Implement SFAM-2349 2
    
    * Implement SFAM-2349 3
    
    * Fix qos class name
    
    * Fix qos class create after cluster activate
    
    * Fix qos default class create
    
    * fix qos cluster setting
    
    * remove cluster level qos config
    
    * fix qos wight list to append zeros
    
    * remove cluster.enable_qos param
    
    fix linter/checks
    
    * fix linter/checks 2
    
    * fix circular import

--- simplyblock_core/controllers/health_controller.py
+++ simplyblock_core/controllers/health_controller.py
@@ -736,6 +736,7 @@ def check_lvol_on_node(lvol_id, node_id, node_bdev_names=None, node_lvols_nqns=N
         passed &= check_subsystem(lvol.nqn, nqns=node_lvols_nqns, ns_uuid=lvol.uuid)
 
     except Exception as e:
+        logger.error(e)
         return False
 
     return passed
