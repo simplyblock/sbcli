6937a7cfc62a3956a59571a154fe60da6aae0dd4
geoffrey1330 <israelgeoffrey13@gmail.com>
2025-10-17 15:44:40 +0100

    use loadbalancer dns_name

--- simplyblock_core/cluster_ops.py
+++ simplyblock_core/cluster_ops.py
@@ -227,9 +227,9 @@ def create_cluster(blk_size, page_size_in_blocks, cli_pass,
     if distr_ndcs == 0 and distr_npcs == 0:
         raise ValueError("both distr_ndcs and distr_npcs cannot be 0")
 
-    if ingress_host_source == "dns":
+    if ingress_host_source == "dns" or ingress_host_source == "loadbalancer":
         if not dns_name:
-            raise ValueError("--dns-name is required when --ingress-host-source is dns")
+            raise ValueError("--dns-name is required when --ingress-host-source is dns or loadbalancer")
 
     logger.info("Installing dependencies...")
     scripts.install_deps(mode)
@@ -316,7 +316,7 @@ def create_cluster(blk_size, page_size_in_blocks, cli_pass,
     if grafana_endpoint:
         cluster.grafana_endpoint = grafana_endpoint
     else:
-        cluster.grafana_endpoint = f"http://{dev_ip}/grafana"
+        cluster.grafana_endpoint = f"http://{dns_name}/grafana"
     cluster.enable_node_affinity = enable_node_affinity
     cluster.qpair_count = qpair_count or constants.QPAIR_COUNT
     cluster.client_qpair_count = client_qpair_count or constants.CLIENT_QPAIR_COUNT
@@ -346,11 +346,11 @@ def create_cluster(blk_size, page_size_in_blocks, cli_pass,
         if not contact_point:
             contact_point = 'https://hooks.slack.com/services/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'
 
-        if not tls_secret:
-            tls_secret = ''
+        # if not tls_secret:
+        #     tls_secret = ''
 
-        if not dns_name:
-            dns_name= ''
+        # if not dns_name:
+        #     dns_name= ''
 
         logger.info("Deploying helm stack ...")
         # log_level = "DEBUG" if constants.LOG_WEB_DEBUG else "INFO"
@@ -366,9 +366,9 @@ def create_cluster(blk_size, page_size_in_blocks, cli_pass,
         #scripts.set_db_config(db_connection)
 
     if not disable_monitoring:
-        _set_max_result_window(dev_ip)
+        _set_max_result_window(dns_name)
 
-        _add_graylog_input(dev_ip, cluster.secret)
+        _add_graylog_input(dns_name, cluster.secret)
 
         _create_update_user(cluster.uuid, cluster.grafana_endpoint, cluster.grafana_secret, cluster.secret)
 
