23ef7be7c354a11afaceb43be0c08660c573b4e9
RaunakJalan <ronakjalan98@gmail.com>
2025-10-18 16:17:42 +0530

    Adding sleep before snapshot id get

--- e2e/e2e_tests/cluster_test_base.py
+++ e2e/e2e_tests/cluster_test_base.py
@@ -133,6 +133,8 @@ class TestClusterBase:
         # fresh folder per run on NFS:
         self.docker_logs_path = os.path.join(self.nfs_log_base, f"{self.test_name}-{timestamp}")
         self.log_path = os.path.join(self.docker_logs_path, "ClientLogs")
+        for node in self.fio_node:
+            self.ssh_obj.make_directory(node=self.log_path)
         
         self.runner_k8s_log = RunnerK8sLog(
                 log_dir=self.docker_logs_path,
