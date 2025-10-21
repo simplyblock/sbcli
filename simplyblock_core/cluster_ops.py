ed784c35392e352eb6cb3cc7be698e22df1a2e5a
geoffrey1330 <israelgeoffrey13@gmail.com>
2025-10-21 20:49:25 +0100

    use cluster secret as monitoring secret in docker mode

--- simplyblock_core/cluster_ops.py
+++ simplyblock_core/cluster_ops.py
@@ -345,6 +345,7 @@ def create_cluster(blk_size, page_size_in_blocks, cli_pass,
         logger.info("Configuring DB...")
         scripts.set_db_config_single()
         logger.info("Configuring DB > Done")
+        monitoring_secret = cluster.secret
 
     elif mode == "kubernetes":
         logger.info("Retrieving foundationdb connection string...")
@@ -355,7 +356,7 @@ def create_cluster(blk_size, page_size_in_blocks, cli_pass,
     if not disable_monitoring:
         if ingress_host_source == "hostip":
             dns_name = dev_ip
-            
+
         _set_max_result_window(dns_name)
 
         _add_graylog_input(dns_name, monitoring_secret)
