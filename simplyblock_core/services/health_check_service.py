f8f512f31367cdb41acd0095751255038af0634d
hamdykhader <hamdy.khader@gmail.com>
2025-10-31 22:40:17 +0300

    Check for sec node leadership when getting jc compression status

--- simplyblock_core/services/health_check_service.py
+++ simplyblock_core/services/health_check_service.py
@@ -237,17 +237,8 @@ while True:
                                             # is_sec_node_leader = True
                                             # check jc_compression status
                                             jc_compression_is_active = second_node_1.rpc_client().jc_compression_get_status(snode.jm_vuid)
-                                            retries = 10
-                                            while jc_compression_is_active:
-                                                if retries <= 0:
-                                                    logger.warning("Timeout waiting for JC compression task to finish")
-                                                    break
-                                                retries -= 1
-                                                logger.info(
-                                                    f"JC compression task found on node: {second_node_1.get_id()}, retrying in 60 seconds")
-                                                time.sleep(60)
-                                                jc_compression_is_active = second_node_1.rpc_client().jc_compression_get_status(snode.jm_vuid)
-                                            lvstore_check &= health_controller._check_sec_node_hublvol(second_node_1, auto_fix=True)
+                                            if not jc_compression_is_active:
+                                                lvstore_check &= health_controller._check_sec_node_hublvol(second_node_1, auto_fix=True)
 
 
                     lvol_port_check = False
