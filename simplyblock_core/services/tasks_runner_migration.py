4db72f447d6fe44847f9f18d6839082b62d013cf
hamdykhader <hamdy.khader@gmail.com>
2025-11-04 11:52:47 +0300

    fix migration condition

--- simplyblock_core/services/tasks_runner_migration.py
+++ simplyblock_core/services/tasks_runner_migration.py
@@ -63,9 +63,9 @@ def task_runner(task):
                     logger.error(f"Failed to get online since: {e}")
 
             for dev in node.nvme_devices:
-                if dev.status in [NVMeDevice.STATUS_ONLINE,
-                                  NVMeDevice.STATUS_FAILED_AND_MIGRATED,
-                                  NVMeDevice.STATUS_CANNOT_ALLOCATE]:
+                if dev.status not in [NVMeDevice.STATUS_ONLINE,
+                                      NVMeDevice.STATUS_FAILED_AND_MIGRATED,
+                                      NVMeDevice.STATUS_CANNOT_ALLOCATE]:
                     task.function_result = f"Some dev status is {dev.status }, retrying"
                     task.status = JobSchedule.STATUS_SUSPENDED
                     task.retry += 1
