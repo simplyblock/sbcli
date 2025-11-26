3fc94cb99e218d46b951d0088334c01f3c1c82f8
hamdykhader <hamdy.khader@gmail.com>
2025-11-26 18:32:27 +0300

    Implements sfam-2459
    Adds the LVolScheduler service definition to docker and k8s deployment scripts
    
    Adds the service creation on cluster update to support update from older cluster
    
    Adds missing service to k8s deployment script (simplyblock-tasks-runner-sync-lvol-del)

---
+++ simplyblock_core/services/lvol_scheduler.py
@@ -0,0 +1,37 @@
+# coding=utf-8
+import time
+
+from simplyblock_core import constants, db_controller, utils
+from simplyblock_core.models.cluster import Cluster
+from simplyblock_core.models.storage_node import StorageNode
+from simplyblock_core.rpc_client import RPCClient
+
+logger = utils.get_logger(__name__)
+
+
+# get DB controller
+db = db_controller.DBController()
+
+logger.info("Starting stats collector...")
+while True:
+
+    for cluster in db.get_clusters():
+
+        if cluster.status in [Cluster.STATUS_INACTIVE, Cluster.STATUS_UNREADY, Cluster.STATUS_IN_ACTIVATION]:
+            logger.warning(f"Cluster {cluster.get_id()} is in {cluster.status} state, skipping")
+            continue
+
+        for snode in db.get_storage_nodes_by_cluster_id(cluster.get_id()):
+
+            lvol_list = db.get_lvols_by_node_id(snode.get_id())
+
+            if not lvol_list:
+                continue
+
+            if snode.status in [StorageNode.STATUS_ONLINE, StorageNode.STATUS_SUSPENDED, StorageNode.STATUS_DOWN]:
+
+                rpc_client = RPCClient(
+                    snode.mgmt_ip, snode.rpc_port,
+                    snode.rpc_username, snode.rpc_password, timeout=3, retry=2)
+
+    time.sleep(constants.LVOL_SCHEDULER_INTERVAL_SEC)
