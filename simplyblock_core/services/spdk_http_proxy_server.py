cdfea25e468c505f47528842b010f89ac9114ab2
Hamdy H. Khader <hamdy.khader@gmail.com>
2025-12-08 16:51:30 +0300

    R25.10 hotfix spdk proxy stats (#812)
    * Add stats to spdk_http_proxy_server.py
    
    Prints max, avg and last 3 sec avg for read lines from http socket and rpc response receive from spdk
    
    * Add stats to spdk_http_proxy_server.py _2
    
    * Fix 2
    
    * Fix 2
    
    * Fix 3

--- simplyblock_core/services/spdk_http_proxy_server.py
+++ simplyblock_core/services/spdk_http_proxy_server.py
@@ -20,6 +20,43 @@ logger = logging.getLogger()
 logger.addHandler(logger_handler)
 logger.setLevel(logging.INFO)
 
+read_line_time_diff: dict = {}
+recv_from_spdk_time_diff: dict = {}
+def print_stats():
+    while True:
+        try:
+            time.sleep(3)
+            t = time.time_ns()
+            read_line_time_diff_max = max(list(read_line_time_diff.values()))
+            read_line_time_diff_avg = int(sum(list(read_line_time_diff.values()))/len(read_line_time_diff))
+            last_3_sec = []
+            for k,v in read_line_time_diff.items():
+                if k > t - 3*1000*1000*1000:
+                    last_3_sec.append(v)
+            if len(last_3_sec) > 0:
+                read_line_time_diff_avg_last_3_sec = int(sum(last_3_sec)/len(last_3_sec))
+            else:
+                read_line_time_diff_avg_last_3_sec = 0
+            logger.info(f"Periodic stats: {t}: read_line_time: max={read_line_time_diff_max} ns, avg={read_line_time_diff_avg} ns, last_3s_avg={read_line_time_diff_avg_last_3_sec} ns")
+            if len(read_line_time_diff) > 10000:
+                read_line_time_diff.clear()
+
+            recv_from_spdk_time_max = max(list(recv_from_spdk_time_diff.values()))
+            recv_from_spdk_time_avg = int(sum(list(recv_from_spdk_time_diff.values()))/len(recv_from_spdk_time_diff))
+            last_3_sec = []
+            for k,v in recv_from_spdk_time_diff.items():
+                if k > t - 3*1000*1000*1000:
+                    last_3_sec.append(v)
+            if len(last_3_sec) > 0:
+                recv_from_spdk_time_avg_last_3_sec = int(sum(last_3_sec)/len(last_3_sec))
+            else:
+                recv_from_spdk_time_avg_last_3_sec = 0
+            logger.info(f"Periodic stats: {t}: recv_from_spdk_time: max={recv_from_spdk_time_max} ns, avg={recv_from_spdk_time_avg} ns, last_3s_avg={recv_from_spdk_time_avg_last_3_sec} ns")
+            if len(recv_from_spdk_time_diff) > 10000:
+                recv_from_spdk_time_diff.clear()
+        except Exception as e:
+            logger.error(e)
+
 
 def get_env_var(name, default=None, is_required=False):
     if not name:
@@ -53,7 +90,7 @@ def rpc_call(req):
     buf = ''
     closed = False
     response = None
-
+    recv_from_spdk_time_start = time.time_ns()
     while not closed:
         newdata = sock.recv(1024*1024*1024)
         if newdata == b'':
@@ -64,6 +101,10 @@ def rpc_call(req):
         except ValueError:
             continue  # incomplete response; keep buffering
         break
+    recv_from_spdk_time_end = time.time_ns()
+    time_diff = recv_from_spdk_time_end - recv_from_spdk_time_start
+    logger.info(f"recv_from_spdk_time_diff: {time_diff}")
+    recv_from_spdk_time_diff[recv_from_spdk_time_start] = time_diff
 
     sock.close()
     unix_sockets.remove(sock)
@@ -103,10 +144,12 @@ class ServerHandler(BaseHTTPRequestHandler):
     def do_POST(self):
         req_time = time.time_ns()
         self.server_session.append(req_time)
+        logger.info(f"incoming request at: {req_time}")
         logger.info(f"active server session: {len(self.server_session)}")
         if self.headers['Authorization'] != 'Basic ' + self.key:
             self.do_AUTHHEAD()
         else:
+            read_line_time_start = time.time_ns()
             if "Content-Length" in self.headers:
                 data_string = self.rfile.read(int(self.headers['Content-Length']))
             elif "chunked" in self.headers.get("Transfer-Encoding", ""):
@@ -126,7 +169,10 @@ class ServerHandler(BaseHTTPRequestHandler):
                     # Finally, a chunk size of 0 is an end indication
                     if chunk_length == 0:
                         break
-
+            read_line_time_end = time.time_ns()
+            time_diff = read_line_time_end - read_line_time_start
+            logger.info(f"read_line_time_diff: {time_diff}")
+            read_line_time_diff[read_line_time_start] = time_diff
             try:
                 response = rpc_call(data_string)
                 if response is not None:
@@ -143,7 +189,8 @@ class ServerHandler(BaseHTTPRequestHandler):
 def run_server(host, port, user, password, is_threading_enabled=False):
     # encoding user and password
     key = base64.b64encode((user+':'+password).encode(encoding='ascii')).decode('ascii')
-
+    print_stats_thread = threading.Thread(target=print_stats, )
+    print_stats_thread.start()
     try:
         ServerHandler.key = key
         httpd = (ThreadingHTTPServer if is_threading_enabled else HTTPServer)((host, port), ServerHandler)
