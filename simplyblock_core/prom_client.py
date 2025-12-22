0438baf8a4e65f1a0c861d70fe10791b99b903a1
hamdykhader <hamdy.khader@gmail.com>
2025-12-22 18:07:56 +0300

    Fix prom client cluster ip in case of k8s

--- simplyblock_core/prom_client.py
+++ simplyblock_core/prom_client.py
@@ -20,13 +20,16 @@ class PromClient:
     def __init__(self, cluster_id):
         db_controller = DBController()
         cluster_ip = None
-        for node in db_controller.get_mgmt_nodes():
-            if node.cluster_id == cluster_id and node.status == MgmtNode.STATUS_ONLINE:
-                cluster_ip = node.mgmt_ip
-                break
-        if cluster_ip is None:
-            raise PromClientException("Cluster has no online mgmt nodes")
-
+        cluster = db_controller.get_cluster_by_id(cluster_id)
+        if cluster.mode == "docker":
+            for node in db_controller.get_mgmt_nodes():
+                if node.cluster_id == cluster_id and node.status == MgmtNode.STATUS_ONLINE:
+                    cluster_ip = node.mgmt_ip
+                    break
+            if cluster_ip is None:
+                raise PromClientException("Cluster has no online mgmt nodes")
+        else:
+            cluster_ip = "simplyblock-prometheus"
         self.ip_address = f"{cluster_ip}:9090"
         self.url = 'http://%s/' % self.ip_address
         self.client = PrometheusConnect(url=self.url, disable_ssl=True)
