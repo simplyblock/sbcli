be6021aee40d4740dd7f09be30b97c6d77ad58e0
hamdykhader <hamdy.khader@gmail.com>
2026-01-08 16:35:24 +0300

    Fix node auto restart
    Allow auto restart if offline nodes == cluster.npcs
    do not restart if cluster is suspended

--- simplyblock_core/controllers/tasks_controller.py
+++ simplyblock_core/controllers/tasks_controller.py
@@ -145,10 +145,13 @@ def add_node_to_auto_restart(node):
                               Cluster.STATUS_READONLY, Cluster.STATUS_UNREADY]:
         logger.warning(f"Cluster is not active, skip node auto restart, status: {cluster.status}")
         return False
+    offline_nodes = 0
     for sn in db.get_storage_nodes_by_cluster_id(node.cluster_id):
         if node.get_id() != sn.get_id() and sn.status != StorageNode.STATUS_ONLINE and node.mgmt_ip != sn.mgmt_ip:
-            logger.info("Node found that is not online, skip node auto restart")
-            return False
+            offline_nodes += 1
+    if offline_nodes >= cluster.distr_npcs :
+        logger.info("Node found that is not online, skip node auto restart")
+        return False
     return _add_task(JobSchedule.FN_NODE_RESTART, node.cluster_id, node.get_id(), "", max_retry=11)
 
 
