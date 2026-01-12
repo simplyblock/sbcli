5b4242c98e541411f11cb1b217f3812e48a112f2
geoffrey1330 <israelgeoffrey13@gmail.com>
2026-01-12 13:46:53 +0100

    make node_address param optional during snode restart via api

--- simplyblock_web/api/v2/storage_node.py
+++ simplyblock_web/api/v2/storage_node.py
@@ -216,7 +216,8 @@ def shutdown(cluster: Cluster, storage_node: StorageNode, force: bool = False) -
 class _RestartParams(BaseModel):
     force: bool = False
     reattach_volume: bool = False
-    node_address: Annotated[str, Field(web_utils.IP_PATTERN)]
+    node_address: Optional[Annotated[str, Field(pattern=web_utils.IP_PATTERN)]] = None
+
 
 
 @instance_api.post('/start', name='clusters:storage-nodes:start', status_code=202, responses={202: {"content": None}})  # Same as restart for now
