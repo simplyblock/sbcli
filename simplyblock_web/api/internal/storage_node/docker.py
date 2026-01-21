a82c0844909b1e944190ca6308d43b15076fd83c
hamdykhader <hamdy.khader@gmail.com>
2026-01-21 22:29:02 +0300

    fix checks 2

--- simplyblock_web/api/internal/storage_node/docker.py
+++ simplyblock_web/api/internal/storage_node/docker.py
@@ -143,7 +143,6 @@ class SPDKParams(BaseModel):
     cluster_mode: str
     socket: Optional[int] = Field(None, ge=0)
     cluster_id: str
-    socket: Optional[int] = Field(None, ge=0)
     firewall_port: int = Field(constants.FW_PORT_START)
 
 
