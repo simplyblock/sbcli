1d3213b865fe9783aa2431d8382b226d4d11acce
schmidt-scaled <82834682+schmidt-scaled@users.noreply.github.com>
2026-02-03 21:27:26 +0300

    Update storage_node_ops.py (#866)

--- simplyblock_core/storage_node_ops.py
+++ simplyblock_core/storage_node_ops.py
@@ -3414,7 +3414,7 @@ def add_lvol_thread(lvol, snode, lvol_ana_state="optimized"):
             logger.error(msg)
             return False, msg
 
-    logger.info("Add BDev to subsystem")
+    logger.info("Add BDev to subsystem "+hex(lvol.vuid))
     ret = rpc_client.nvmf_subsystem_add_ns(lvol.nqn, lvol.top_bdev, lvol.uuid, lvol.guid, nsid=lvol.ns_id, eui64=hex(lvol.vuid))
     for iface in snode.data_nics:
         if iface.ip4_address and lvol.fabric == iface.trtype.lower():
