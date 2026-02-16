319b56db081a5311c16edabf5ee88927cb314a3f
Hamdy H. Khader <hamdy.khader@gmail.com>
2026-02-16 14:50:01 +0300

    Main clone new size (#878)
    * Ceil new_size to the nearest GiB in snapshot resizing
    
    * Ensure new_size is strictly greater than the original size in snapshot resizing

--- simplyblock_core/controllers/snapshot_controller.py
+++ simplyblock_core/controllers/snapshot_controller.py
@@ -498,7 +498,7 @@ def clone(snapshot_id, clone_name, new_size=0, pvc_name=None, pvc_namespace=None
 
     if new_size:
         new_size = math.ceil(new_size / (1024 * 1024 * 1024)) * 1024 * 1024 * 1024
-        if snap.lvol.size >= new_size:
+        if snap.lvol.size > new_size:
             msg = f"New size {new_size} must be higher than the original size {snap.lvol.size}"
             logger.error(msg)
             return False, msg
