59135cd07eabf70675bc38e0f562962a54619b47
Geoffrey Israel <israelgeoffrey13@gmail.com>
2026-02-27 15:54:44 +0100

    Resize clone newsize greater (#898)
    * resize clone if new size larger than original size
    
    * resize clone if new size larger than original size
    
    * avoid name error
    
    (cherry picked from commit 9419234e2f5cade7d27ed204d6677dd0ade9a594)

--- simplyblock_core/controllers/snapshot_controller.py
+++ simplyblock_core/controllers/snapshot_controller.py
@@ -505,6 +505,7 @@ def clone(snapshot_id, clone_name, new_size=0, pvc_name=None, pvc_namespace=None
         lvol.crypto_key1 = snap.lvol.crypto_key1
         lvol.crypto_key2 = snap.lvol.crypto_key2
 
+    conv_new_size = None  
     if new_size:
         conv_new_size = math.ceil(new_size / (1024 * 1024 * 1024)) * 1024 * 1024 * 1024
         if snap.lvol.size > conv_new_size:
@@ -604,7 +605,7 @@ def clone(snapshot_id, clone_name, new_size=0, pvc_name=None, pvc_namespace=None
 
     logger.info("Done")
     snapshot_events.snapshot_clone(snap, lvol)
-    if new_size:
+    if new_size and conv_new_size > snap.lvol.size:
         lvol_controller.resize_lvol(lvol.get_id(), new_size)
     return lvol.uuid, False
 
