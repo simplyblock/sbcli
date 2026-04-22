29bb4a7cc8206bb5bf5a49ccab6106345d61a48c
hamdykhader <hamdy.khader@gmail.com>
2026-04-23 00:29:57 +0300

    docker: use sudo -E for environment preservation in service scripts

--- simplyblock_web/api/internal/storage_node/docker.py
+++ simplyblock_web/api/internal/storage_node/docker.py
@@ -199,7 +199,7 @@ def spdk_process_start(body: SPDKParams):
     )
     node_docker.containers.run(
         body.spdk_proxy_image,
-        "sudo python3 simplyblock_core/services/spdk_http_proxy_server.py ",
+        "sudo -E python3 simplyblock_core/services/spdk_http_proxy_server.py ",
         name=f"spdk_proxy_{body.rpc_port}",
         detach=True,
         network_mode="host",
