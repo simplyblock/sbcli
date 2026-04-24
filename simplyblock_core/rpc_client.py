91447809c3f4e86f26a7705f8540f3ad94880928
ebrahim savari <106956085+EbiRider@users.noreply.github.com>
2026-04-24 14:56:33 +0330

    trying a new paramter to solve spdk unparsable json issue

--- simplyblock_core/rpc_client.py
+++ simplyblock_core/rpc_client.py
@@ -1466,7 +1466,7 @@ class RPCClient:
         """
         return self._request("bdev_lvol_final_migration", {
             "lvol_name": lvol_name,
-            "blobid": lvol_id,
+            "map_id": lvol_id,
             "snapshot_name": snapshot_name,
             "cluster_batch": batch_size,
             "gateway": bdev_name,
