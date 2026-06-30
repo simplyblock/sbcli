simplyblock/sbcli

commit 3fd727862db6a263866c8e57ecde3dd00d7b31ce

Introduce helpers for key names

simplyblock_core/controllers/snapshot_controller.py
@@ -11,7 +11,7 @@ from simplyblock_core.controllers import lvol_controller, snapshot_events, pool_
 
 from simplyblock_core import utils
 from simplyblock_core.exceptions import PreconditionError
-from simplyblock_core.kms import create_kms_connection
+from simplyblock_core.kms import create_kms_connection, dek_name, kek_name
 from simplyblock_core.kms._exceptions import KMSException
 from simplyblock_core.db_controller import DBController
 from simplyblock_core.models.job_schedule import JobSchedule
@@ -846,14 +846,18 @@ def clone(snapshot_id, clone_name, new_size=0, pvc_name=None, pvc_namespace=None
             return False, msg
 
     if snap.lvol.crypto_bdev:
+        lvol.crypto_bdev = f"crypto_{lvol.lvol_bdev}"
+        lvol.bdev_stack.append({"type": "crypto"})
+        lvol.lvol_type += ',crypto'
+        lvol.top_bdev = lvol.crypto_bdev
         with create_kms_connection(cluster) as kms:
             try:
-                key1, key2 = kms.get_data_encryption_keys(snap.lvol)
-                kms.import_data_encryption_keys(lvol, (key1, key2))
-                lvol.crypto_bdev = f"crypto_{lvol.lvol_bdev}"
-                lvol.bdev_stack.append({"type": "crypto"})
-                lvol.lvol_type += ',crypto'
-                lvol.top_bdev = lvol.crypto_bdev
+                key1, key2 = kms.get_data_encryption_keys(
+                    dek_name(cluster.get_id(), snap.lvol), kek_name(pool.get_id()),
+                )
+                kms.import_data_encryption_keys(
+                    dek_name(cluster.get_id(), lvol), kek_name(pool.get_id()), (key1, key2),
+                )
             except KMSException:
                 msg = f"Failed to copy encryption keys for clone {lvol.crypto_bdev}"
                 logger.exception(msg)

simplyblock_core/controllers/backup_controller.py
@@ -11,7 +11,7 @@ from simplyblock_core.controllers import backup_events, tasks_controller
 from simplyblock_core.db_controller import DBController
 from simplyblock_core.models.backup import Backup, BackupPolicy, BackupPolicyAttachment
 from simplyblock_core.models.storage_node import StorageNode
-from simplyblock_core.kms import KMSException, create_kms_connection
+from simplyblock_core.kms import KMSException, create_kms_connection, dek_name, kek_name
 from simplyblock_core.utils.secrets import unwrap_secret as _unwrap_secret
 
 logger = logging.getLogger()
@@ -453,7 +453,9 @@ def restore_backup(backup_id, lvol_name, pool_id_or_name, cluster_id=None,
 
     with create_kms_connection(db_controller.get_cluster_by_id(backup.cluster_id)) as kms:
         try:
-            key1, key2 = kms.get_data_encryption_keys(original_lvol.crypto_bdev, original_lvol.pool_uuid)
+            key1, key2 = kms.get_data_encryption_keys(
+                dek_name(backup.cluster_id, original_lvol), kek_name(original_lvol.pool_uuid),
+            )
         except KMSException:
             return None, "Failed to retrieve original crypto keys"
 

simplyblock_core/controllers/lvol_controller.py
@@ -12,7 +12,7 @@ from simplyblock_core.controllers import snapshot_controller, pool_controller, l
     snapshot_events
 from simplyblock_core.db_controller import DBController
 from simplyblock_core.exceptions import PreconditionError
-from simplyblock_core.kms import KMSException, create_kms_connection
+from simplyblock_core.kms import KMSException, create_kms_connection, dek_name, kek_name
 from simplyblock_core.controllers.host_auth import (
     _get_dhchap_group, _register_dhchap_keys_on_node, _register_pool_dhchap_keys_on_node)
 from simplyblock_core.models.cluster import Cluster
@@ -45,7 +45,9 @@ def _create_crypto_lvol(rpc_client, lvol, cluster):
 
     with create_kms_connection(cluster) as kms:
         try:
-            original_key1, original_key2 = kms.get_data_encryption_keys(lvol.bdev_lvol, lvol.pool_uuid)
+            original_key1, original_key2 = kms.get_data_encryption_keys(
+                dek_name(cluster.get_id(), lvol), kek_name(lvol.pool_uuid),
+            )
         except KMSException:
             logger.exception(f"Failed to get keys for lvol: {name} from KMS")
             return False
@@ -580,9 +582,11 @@ def add_lvol_ha(name, size, host_id_or_name, ha_type, pool_id_or_name, use_comp=
         with create_kms_connection(cl) as kms:
             try:
                 if crypto_key is None:
-                    kms.create_data_encryption_keys(lvol.crypto_bdev, lvol.pool_uuid)
+                    kms.create_data_encryption_keys(dek_name(cl.get_id(), lvol), kek_name(pool.get_id()))
                 else:
-                    kms.import_data_encryption_keys(lvol.crypto_bdev, lvol.pool_uuid, crypto_key)
+                    kms.import_data_encryption_keys(
+                        dek_name(cl.get_id(), lvol), kek_name(pool.get_id()), crypto_key,
+                    )
                 logger.info("Created lvol keys")
             except KMSException:
                 msg = "Failed to create lvol keys"
@@ -1458,7 +1462,7 @@ def delete_lvol(lvol: LVol, *, force_delete: bool = False) -> None:
     if lvol.crypto_bdev:
         with create_kms_connection(cl) as kms:
             try:
-                kms.delete_data_encryption_keys(lvol.crypto_bdev)
+                kms.delete_data_encryption_keys(dek_name(cl.get_id(), lvol))
                 logger.info("Deleted lvol key")
             except KMSException:
                 logger.exception("Failed to delete lvol key")

simplyblock_core/controllers/migration_controller.py
@@ -47,7 +47,7 @@ from simplyblock_core import constants
 from simplyblock_core.controllers import migration_events, tasks_controller, snapshot_controller
 from simplyblock_core.exceptions import MigrationConflictError, PreconditionError
 from simplyblock_core.controllers.host_auth import _reapply_allowed_hosts
-from simplyblock_core.kms import create_kms_connection
+from simplyblock_core.kms import create_kms_connection, dek_name, kek_name
 from simplyblock_core.db_controller import DBController
 from simplyblock_core.models.lvol_migration import LVolMigration
 from simplyblock_core.models.lvol_model import LVol
@@ -827,7 +827,9 @@ def create_migration(lvol_id, target_node_id,
             else:
                 try:
                     with create_kms_connection(cluster) as kms:
-                        _key1, _key2 = kms.get_data_encryption_keys(lvol)
+                        _key1, _key2 = kms.get_data_encryption_keys(
+                            dek_name(cluster.get_id(), lvol), kek_name(lvol.pool_uuid),
+                        )
                     _key_name = f"key_{_crypto_short}"
                     _ret = _rpc.lvol_crypto_key_create(_key_name, _key1, _key2)
                     if not _ret:

simplyblock_core/controllers/pool_controller.py
@@ -12,7 +12,7 @@ from pydantic import SecretStr
 from simplyblock_core import utils
 from simplyblock_core.controllers import pool_events, lvol_controller
 from simplyblock_core.db_controller import DBController
-from simplyblock_core.kms import KMSException, create_kms_connection
+from simplyblock_core.kms import KMSException, create_kms_connection, kek_name
 from simplyblock_core.models.pool import Pool
 from simplyblock_core.prom_client import PromClient
 
@@ -105,7 +105,7 @@ def add_pool(name, pool_max, lvol_max, max_rw_iops, max_rw_mbytes, max_r_mbytes,
 
     try:
         with create_kms_connection(cluster) as kms:
-            kms.create_key_encryption_key(pool.get_id())
+            kms.create_key_encryption_key(kek_name(pool.get_id()))
             logger.info("Created pool key")
     except KMSException:
         logger.exception("Failed to create pool key")
@@ -360,7 +360,7 @@ def delete_pool(uuid):
 
     with create_kms_connection(cluster) as kms:
         try:
-            kms.delete_key_encryption_key(pool.get_id())
+            kms.delete_key_encryption_key(kek_name(pool.get_id()))
             logger.info("Deleted pool key")
         except KMSException:
             logger.exception("Failed to delete pool key")

simplyblock_core/kms/__init__.py
@@ -1,6 +1,7 @@
 import logging
 
 from simplyblock_core.models.cluster import Cluster
+from simplyblock_core.models.lvol_model import LVol
 from simplyblock_core.settings import Settings
 
 from ._base import KMS
@@ -11,6 +12,14 @@ from ._fdb import LocalKMS
 logger = logging.getLogger()
 
 logger.setLevel(logging.DEBUG)
+
+
+def dek_name(cluster_id: str, lvol: LVol) -> str:
+    return f"{cluster_id}/{lvol.crypto_bdev}"
+
+
+def kek_name(pool_id: str) -> str:
+    return pool_id
 
 
 def create_kms_connection(cluster: Cluster) -> KMS:
@@ -35,7 +44,6 @@ def create_kms_connection(cluster: Cluster) -> KMS:
         settings.tls_certificate_authority,
         settings.tls_certificate,
         settings.tls_key,
-        cluster.get_id(),
         transit_mount=vault.transit_mount,
         kv_mount=vault.kv_mount,
         cert_role=vault.cert_role,

simplyblock_core/kms/_hcp.py
@@ -1,7 +1,6 @@
 import base64
 import logging
 from pathlib import Path
-from uuid import UUID
 
 import hvac
 import hvac.exceptions
@@ -18,14 +17,12 @@ class HCPClient(KMS):
         tls_certificate_authority: Path,
         tls_certificate: Path,
         tls_key: Path,
-        cluster_id: UUID,
         transit_mount: str,
         kv_mount: str,
         cert_role: str,
         timeout: int = 300,
         retry: int = 5,
     ):
-        self.cluster_id = cluster_id
         self.transit_mount = transit_mount
         self.kv_mount = kv_mount
         self.client = hvac.Client(
@@ -74,7 +71,7 @@ class HCPClient(KMS):
     def create_data_encryption_keys(self, name: str, kek_name: str) -> None:
         try:
             self.client.secrets.kv.v2.create_or_update_secret(
-                path=f"{self.cluster_id}/{name}",
+                path=name,
                 secret={"keys": [
                     self._create_data_encryption_key(kek_name),
                     self._create_data_encryption_key(kek_name),
@@ -87,7 +84,7 @@ class HCPClient(KMS):
     def import_data_encryption_keys(self, name: str, kek_name: str, keys: tuple[str, str]) -> None:
         try:
             self.client.secrets.kv.v2.create_or_update_secret(
-                path=f"{self.cluster_id}/{name}",
+                path=name,
                 secret={"keys": [
                     self._encrypt(kek_name, keys[0]),
                     self._encrypt(kek_name, keys[1]),
@@ -100,7 +97,7 @@ class HCPClient(KMS):
     def get_data_encryption_keys(self, name: str, kek_name: str) -> tuple[str, str]:
         try:
             encrypted_key1, encrypted_key2 = self.client.secrets.kv.v2.read_secret_version(
-                path=f"{self.cluster_id}/{name}",
+                path=name,
                 mount_point=self.kv_mount,
             )['data']['data']['keys']
             return (
@@ -113,7 +110,7 @@ class HCPClient(KMS):
     def delete_data_encryption_keys(self, name: str) -> None:
         try:
             self.client.secrets.kv.v2.delete_metadata_and_all_versions(
-                path=f"{self.cluster_id}/{name}",
+                path=name,
                 mount_point=self.kv_mount,
             )
         except hvac.exceptions.VaultError as e:

commit 1c111b6e25d7480e80c77c00d7e292428dab81c8

fix(clone): include name+params in crypto bdev_stack entry (cloning encrypted volume)
Cloning an encrypted volume raised `KeyError: 'name'` in
lvol_controller._create_bdev_stack: the clone path appended a crypto stack
entry as `{"type": "crypto"}` only, but _create_bdev_stack reads
bdev['name'] and bdev['params'] for every entry.

Mirror the create path (lvol_controller.add_lvol): append the crypto entry with
name = lvol.crypto_bdev and params {name, base_name}, where base_name is the
current top_bdev (the clone bdev) captured before top_bdev is reassigned to the
crypto bdev. Non-encrypted clones were unaffected (no crypto entry appended).

Regression introduced in 3fd727862 (2026-06-23).

Co-Authored-By: Claude Opus 4.8 <noreply@anthropic.com>

simplyblock_core/controllers/snapshot_controller.py
@@ -1011,7 +1011,20 @@ def clone(snapshot_id, clone_name, new_size=0, pvc_name=None, pvc_namespace=None
 
     if snap.lvol.crypto_bdev:
         lvol.crypto_bdev = f"crypto_{lvol.lvol_bdev}"
-        lvol.bdev_stack.append({"type": "crypto"})
+        # The crypto stack entry must carry name + params: _create_bdev_stack
+        # reads bdev['name']/bdev['params'] for every entry. Appending only
+        # {"type": "crypto"} raised KeyError: 'name' when cloning an encrypted
+        # volume. Mirror the create path (lvol_controller.add_lvol). base_name
+        # is the current top_bdev (the clone bdev) — captured before top_bdev is
+        # reassigned to the crypto bdev below.
+        lvol.bdev_stack.append({
+            "type": "crypto",
+            "name": lvol.crypto_bdev,
+            "params": {
+                "name": lvol.crypto_bdev,
+                "base_name": lvol.top_bdev,
+            },
+        })
         lvol.lvol_type += ',crypto'
         lvol.top_bdev = lvol.crypto_bdev
         with create_kms_connection(cluster) as kms: