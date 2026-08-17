e2e0a0d49f4202db1a3fa640849bfced0271083d
Max Schettler <max@simplyblock.io>
2026-08-17 12:37:39 +0200

    Fix linter error

--- simplyblock_core/services/snapshot_monitor.py
+++ simplyblock_core/services/snapshot_monitor.py
@@ -8,7 +8,7 @@ from datetime import datetime
 from simplyblock_core import constants, db_controller, utils
 from simplyblock_core.models.cluster import Cluster
 from simplyblock_core.controllers import (
-    snapshot_events, snapshot_controller, tasks_controller)
+    snapshot_events, snapshot_controller)
 from simplyblock_core.models.lvol_model import LVol
 from simplyblock_core.models.snapshot import SnapShot
 from simplyblock_core.models.storage_node import StorageNode
