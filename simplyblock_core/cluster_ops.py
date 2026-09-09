231834f9add488f6469e33bd44d918fdf1ae39ec
hamdykhader <hamdy.khader@gmail.com>
2026-09-09 17:42:04 +0300

    fix(cluster_ops): update service environment variable for logging level

--- simplyblock_core/cluster_ops.py
+++ simplyblock_core/cluster_ops.py
@@ -2919,7 +2919,11 @@ def update_cluster(cluster_id, mgmt_only=False, restart=False, spdk_image=None, 
                         service.remove()
                     else:
                         logger.info(f"Updating service {service.name}")
-                        service.update(image=service_image, force_update=True)
+                        service_env = service.attrs['Spec']['TaskTemplate']['ContainerSpec']['Env']
+                        if "SIMPLYBLOCK_LOG_LEVEL=DEBUG" in service_env:
+                            service_env.remove("SIMPLYBLOCK_LOG_LEVEL=DEBUG")
+                            service_env.add("SIMPLYBLOCK_LOG_LEVEL=INFO")
+                        service.update(image=service_image, env=service_env, force_update=True)
                         service_names.append(service.attrs['Spec']['Name'])
                     break
 
