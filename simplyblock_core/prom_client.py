a94e211a620c9c0d4c4afe486629cf5a269298c6
Hamdy H. Khader <hamdy.khader@gmail.com>
2026-09-18 17:50:01 +0300

    R26.3 fix metric name (#1368) (#1377)
    * fix(prom_client): simplify metric data structure by removing redundant metric key

--- simplyblock_core/prom_client.py
+++ simplyblock_core/prom_client.py
@@ -143,16 +143,14 @@ class PromClient:
             except Exception:
                 raise PromClientException(f"Error parsing history string: {history}")
         end_time = datetime.now()
-        data_out: list[tuple] = []
+        data_out: list[dict] = []
         metrics = self.client.get_metric_range_data(
             metric_key, label_config=params, start_time=start_time, end_time=end_time)
         for m in metrics:
-            print(m)
-            mt_name = metric_key
             mt_info = m["metric"]
             mt_values = m["values"]
-            mt_values_list = [v for i, v in enumerate(mt_values)]
-            data_out.append((mt_name, mt_info, mt_values_list))
+            mt_values_list = [v[1] for v in mt_values]
+            data_out.append({"key": metric_key,"metric": mt_info, "values": mt_values_list})
         return data_out
 
     def get_node_filesystem_metrics(self, history=None):
@@ -166,10 +164,13 @@ class PromClient:
         try:
             for metric in metrics_lst:
                 params[metric] = ""
-                response_list= self.get_raw_metric(f"node_filesystem_{metric}", params, history)
-                for m, v in response_list:
-                    node_name = m["instance"]
-                    node_stats[node_name] = {metric: v}
+                response_list = self.get_raw_metric(f"node_filesystem_{metric}", params, history)
+                for m_data in response_list:
+                    node_name = m_data["metric"]["instance"]
+                    if node_name not in node_stats:
+                        node_stats[node_name] = {metric: m_data["values"]}
+                    else:
+                        node_stats[node_name][metric] = m_data["values"]
         except Exception as e:
             logger.error(f"Error getting node filesystem metrics: {e}")
             return []
@@ -184,10 +185,13 @@ class PromClient:
         try:
             for metric in metrics_lst:
                 params[metric] = ""
-                response_list= self.get_raw_metric(f"http_request_duration_{metric}", params, history)
-                for m, v in response_list:
-                    node_name = m["instance"]
-                    api_stats[node_name] = {metric: v}
+                response_list = self.get_raw_metric(f"http_request_duration_{metric}", params, history)
+                for m_data in response_list:
+                    node_name = m_data["metric"]["instance"]
+                    if node_name not in api_stats:
+                        api_stats[node_name] = {metric: m_data["values"]}
+                    else:
+                        api_stats[node_name][metric] = m_data["values"]
         except Exception as e:
             logger.error(f"Error getting API metrics: {e}")
             return []
